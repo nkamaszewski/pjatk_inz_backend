9952b268c5275468d05ede02c337cf0ddc8da393
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2021-05-26 18:11:07 +0200

    poprawki - Model

--- model/sequelize/ApplicationForRefund.js
+++ model/sequelize/ApplicationForRefund.js
@@ -3,9 +3,10 @@ const sequelize = require('../../config/sequelize/sequelize');
 
 const ApplicationForRefund = sequelize.define('ApplicationForRefund', {
     idApplicationForRefund: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-	idApplicationFor: { type: Sequelize.INTEGER, allowNull: false},
-	IdStatus: { type: Sequelize.INTEGER, allowNull: false }
-	}, {
+    idApplicationFor: { type: Sequelize.INTEGER, allowNull: false },
+    IdStatus: { type: Sequelize.INTEGER, allowNull: false },
+    DateOfSubmission: { type: Sequelize.DATE, allowNull: false }
+}, {
     timestamps: false,
     tableName: 'ApplicationForRefund'
 });
--- model/sequelize/Employee.js
+++ model/sequelize/Employee.js
@@ -3,8 +3,9 @@ const sequelize = require('../../config/sequelize/sequelize');
 
 const Employee = sequelize.define('Employee', {
     idEmployee: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-	Pesel: { type: Sequelize.STRING, allowNull: false }
-	}, {
+    Pesel: { type: Sequelize.STRING, allowNull: false },
+    Password: { type: Sequelize.STRING, allowNull: false }
+}, {
     timestamps: false,
     tableName: 'Employee'
 });
