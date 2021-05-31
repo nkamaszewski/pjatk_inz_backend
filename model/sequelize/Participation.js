49d61e4089c49f26c633e68db2213c8aaec1b226
karol <s17580@pjwstk.edu.pl>
2021-05-31 22:56:11 +0200

    Poprawki model

--- model/sequelize/Participation.js
+++ model/sequelize/Participation.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 const sequelize = require('../../config/sequelize/sequelize');
 
 const Participation = sequelize.define('Participation', {
-    idParticipation: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-	idPerson: { type: Sequelize.INTEGER, allowNull: false},
-	idEducation: { type: Sequelize.INTEGER, allowNull: false},
+    IdParticipation: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
+	IdPerson: { type: Sequelize.INTEGER, allowNull: false},
+	IdEducation: { type: Sequelize.INTEGER, allowNull: false},
 	DateOfRegistration: { type: Sequelize.DATE, allowNull: false},
 	EndDate: { type: Sequelize.DATE, allowNull: true},
 	CertificateOfCompletion: { type: Sequelize.STRING, allowNull: true}
--- model/sequelize/Questionnaire.js
+++ model/sequelize/Questionnaire.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 const sequelize = require('../../config/sequelize/sequelize');
 
 const Questionnaire = sequelize.define('Questionnaire', {
-    idQuestionnaire: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-	idParticipation: { type: Sequelize.INTEGER, allowNull: false},
+    IdQuestionnaire: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
+	IdParticipation: { type: Sequelize.INTEGER, allowNull: false},
 	Date: { type: Sequelize.DATE, allowNull: false}
 	}, {
     timestamps: false,
