51dac6051ba30f105dc3eff503943f25bb42bf85
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2021-07-24 23:18:04 +0200

    dodane indexy Person

--- model/sequelize/Person.js
+++ model/sequelize/Person.js
@@ -7,8 +7,22 @@ const Person = sequelize.define('Person', {
     LastName: { type: Sequelize.STRING, allowNull: false },
     Email: { type: Sequelize.STRING, allowNull: false },
     Phone: { type: Sequelize.STRING, allowNull: false }
-}, {
-    timestamps: false,
-    tableName: 'Person'
-});
+},
+    {
+        timestamps: false,
+        tableName: 'Person',
+        indexes: [
+            {
+                name: 'idx_person_email',
+                unique: true,
+                fields: ['Email']
+            },
+            {
+                name: 'idx_person_LastName_FirstName',
+                fields: ['LastName', 'FirstName']
+            }
+        ]
+    }
+);
+
 module.exports = Person;
