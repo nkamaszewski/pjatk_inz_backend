be95ba062116c0f8d5c48ac02a37c06b9facf819
karol <s17580@pjwstk.edu.pl>
2021-08-01 12:43:03 +0200

    poprawa modelu

--- model/sequelize/QuestionnaireOffer.js
+++ model/sequelize/QuestionnaireOffer.js
@@ -4,7 +4,7 @@ const sequelize = require('../../config/sequelize/sequelize');
 const QuestionnaireOffer = sequelize.define('QuestionnaireOffer', {
     IdQuestionnaireOffer: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
     Year: { type: Sequelize.INTEGER, allowNull: false },
-    idPerson: { type: Sequelize.INTEGER, allowNull: false }
+    IdPerson: { type: Sequelize.INTEGER, allowNull: false }
 }, {
     timestamps: false,
     tableName: 'QuestionnaireOffer'
