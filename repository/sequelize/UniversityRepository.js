9f9fb6ded0d1407f19ab1e29445ba910773167eb
Norbert Kamaszewski <norbert.kamaszewski@gmail.com>
2021-08-05 14:39:31 +0200

    poprawka w dodawaniu uczelni

--- repository/sequelize/UniversityRepository.js
+++ repository/sequelize/UniversityRepository.js
@@ -2,32 +2,40 @@ const University = require('../../model/sequelize/University');
 const Study = require('../../model/sequelize/Study');
 
 exports.getUniversitys = () => {
-    return University.findAll();
+  return University.findAll();
 };
 
 exports.createUniversity = (newUniversityData) => {
-    return University.create({
-        University: newUniversityData.University
-    });
+  const { Name, ShortName, City, PostalCode, Street, Number } =
+    newUniversityData;
+  return University.create({
+    Name,
+    ShortName,
+    City,
+    PostalCode,
+    Street,
+    Number,
+  });
 };
 
 exports.deleteUniversity = (universityId) => {
-    return University.destroy({
-        where: { IdUniversity: universityId }
-    });
+  return University.destroy({
+    where: { IdUniversity: universityId },
+  });
 };
 
 exports.updateUniversity = (universityId, data) => {
-    const university = data.University;
-    return University.update(data, { where: { IdUniversity: universityId } });
-}
+  const university = data.University;
+  return University.update(data, { where: { IdUniversity: universityId } });
+};
 
 exports.getUniversityById = (univId) => {
-    return University.findByPk(univId,
-        {
-            include: [{
-                model: Study,
-                as: 'universityStudys'
-            }]
-        });
+  return University.findByPk(univId, {
+    include: [
+      {
+        model: Study,
+        as: 'universityStudys',
+      },
+    ],
+  });
 };
