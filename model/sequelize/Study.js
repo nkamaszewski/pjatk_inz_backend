5c1e7b137b0a828f9c64091d8c785384aa477b32
Norbert Kamaszewski <norbert.kamaszewski@gmail.com>
2021-08-05 20:55:11 +0200

    poprawki dodawanie studia

--- model/sequelize/Study.js
+++ model/sequelize/Study.js
@@ -1,14 +1,23 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../../config/sequelize/sequelize');
 
-const Study = sequelize.define('Study', {
-    IdEducation: { type: Sequelize.INTEGER, autoIncrement: false, allowNull: false, primaryKey: true },
+const Study = sequelize.define(
+  'Study',
+  {
+    IdEducation: {
+      type: Sequelize.INTEGER,
+      autoIncrement: false,
+      allowNull: false,
+      primaryKey: true,
+    },
     FieldOfStudy: { type: Sequelize.STRING, allowNull: false },
     IdUniversity: { type: Sequelize.INTEGER, allowNull: false },
-    IdMode: { type: Sequelize.INTEGER, allowNull: false },
-    IdGradueDegree: { type: Sequelize.INTEGER, allowNull: false },
-}, {
+    IdStudyMode: { type: Sequelize.INTEGER, allowNull: false },
+    IdGraduateDegree: { type: Sequelize.INTEGER, allowNull: false },
+  },
+  {
     timestamps: false,
-    tableName: 'Study'
-});
+    tableName: 'Study',
+  }
+);
 module.exports = Study;
--- repository/sequelize/StudyRepository.js
+++ repository/sequelize/StudyRepository.js
@@ -2,33 +2,38 @@ const University = require('../../model/sequelize/University');
 const Study = require('../../model/sequelize/Study');
 
 exports.getStudys = () => {
-    return Study.findAll({
-        attributes: ['IdStudy', 'FieldOfStudy', 'IdUniversity','IdMode','IdGraduateDegree'],
-        include: [{
-            model: University,
-            as: 'studysUniversity'
-        }]
-    });
+  return Study.findAll({
+    attributes: [
+      'IdEducation',
+      'FieldOfStudy',
+      'IdUniversity',
+      'IdStudyMode',
+      'IdGraduateDegree',
+    ],
+    // include: [
+    //   {
+    //     model: University,
+    //     as: 'studysUniversity',
+    //   },
+    // ],
+  });
 };
 
 exports.createStudy = (newStudyData) => {
-    const {FieldOfStudy} = newStudyData;
-    return Study.create({FieldOfStudy
-        
-    });
+  return Study.create(newStudyData);
 };
 
 exports.deleteStudy = (studyId) => {
-    return Study.destroy({
-        where: { IdStudy: studyId }
-    });
+  return Study.destroy({
+    where: { IdStudy: studyId },
+  });
 };
 
 exports.updateStudy = (studyId, data) => {
-    const FieldOfStudy = data.FieldOfStudy;
-    return Study.update(data, { where: { IdStudy: studyId } });
-}
+  const FieldOfStudy = data.FieldOfStudy;
+  return Study.update(data, { where: { IdStudy: studyId } });
+};
 
 exports.getStudyById = (studId) => {
-    return Study.findByPk(studId);
+  return Study.findByPk(studId);
 };
