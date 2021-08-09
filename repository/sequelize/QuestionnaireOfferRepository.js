1e293fe74e127d20d4f59c4a4eeb7ab1a284b9b5
Norbert Kamaszewski <norbert.kamaszewski@gmail.com>
2021-08-09 23:20:01 +0200

    poprawki questOffId

--- repository/sequelize/QuestionnaireOfferRepository.js
+++ repository/sequelize/QuestionnaireOfferRepository.js
@@ -2,42 +2,42 @@ const Offer = require('../../model/sequelize/Offer');
 const QuestionnaireOffer = require('../../model/sequelize/QuestionnaireOffer');
 
 exports.getQuestionnaireOffers = () => {
-    return QuestionnaireOffer.findAll({
-        attributes: ['IdQuestionnaireOffer', 'Year', 'IdPerson'],
-        include: [{
-            model: Offer,
-            as: 'guestionnaireOfferOffer'
-        }]
-    });
-
+  return QuestionnaireOffer.findAll({
+    attributes: ['IdQuestionnaireOffer', 'Year', 'IdPerson'],
+    include: [
+      {
+        model: Offer,
+        as: 'questionnaireOfferOffer',
+      },
+    ],
+  });
 };
 
 exports.createQuestionnaireOffer = (newQuestionnaireOfferData) => {
-    const { IdQuestionnaireOffer, Year, IdPerson } =
-    newQuestionnaireOfferData;
+  const { IdQuestionnaireOffer, Year, IdPerson } = newQuestionnaireOfferData;
   return QuestionnaireOffer.create({
-    IdQuestionnaireOffer, 
-    Year, 
-    IdPerson
+    IdQuestionnaireOffer,
+    Year,
+    IdPerson,
   });
 };
 
 exports.deleteQuestionnaireOffer = (questionnaireOfferId) => {
-    return QuestionnaireOffer.destroy({
-        where: { IdQuestionnaireOffer: questionnaireOfferId }
-    });
+  return QuestionnaireOffer.destroy({
+    where: { IdQuestionnaireOffer: questionnaireOfferId },
+  });
 };
 
 exports.updateQuestionnaireOffer = (questionnaireOfferId, data) => {
+  const IdQuestionnaireOffer = data.IdQuestionnaireOffer;
+  const IdPerson = data.IdPerson;
+  const Year = data.Year;
 
-    const IdQuestionnaireOffer = data.IdQuestionnaireOffer;
-    const IdPerson = data.IdPerson;
-    const Year = data.Year;
-
-
-    return QuestionnaireOffer.update(data, { where: { IdQuestionnaireOffer: questionnaireOfferId } });
-}
+  return QuestionnaireOffer.update(data, {
+    where: { IdQuestionnaireOffer: questionnaireOfferId },
+  });
+};
 
 exports.getQuestionnaireOfferById = (questionnaireOfferId) => {
-    return QuestionnaireOffer.findByPk(questionnaireOfferId);
+  return QuestionnaireOffer.findByPk(questionnaireOfferId);
 };
