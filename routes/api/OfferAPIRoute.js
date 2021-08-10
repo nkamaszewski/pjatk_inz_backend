72f952bbd7745b980478360a57ac0f7f9cc9b063
Norbert Kamaszewski <norbert.kamaszewski@gmail.com>
2021-08-10 13:35:11 +0200

    poprawka w nazwie parametru offer controllera

--- routes/api/OfferAPIRoute.js
+++ routes/api/OfferAPIRoute.js
@@ -6,6 +6,6 @@ router.get('/', offApiController.getOffers);
 // router.get('/:offId', meetApiController.getOfferById);
 
 router.post('/', offApiController.createOffer);
-router.put('/:offId', offApiController.updateOffer);
-router.delete('/:offId', offApiController.deleteOffer);
+router.put('/:offerId', offApiController.updateOffer);
+router.delete('/:offerId', offApiController.deleteOffer);
 module.exports = router;
