97f4c501809b3c4ff90a2447b3c03b373f301b25
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2021-08-10 22:01:33 +0200

    walidacja dat

--- model/sequelize/Meeting.js
+++ model/sequelize/Meeting.js
@@ -3,8 +3,27 @@ const sequelize = require('../../config/sequelize/sequelize');
 
 const Meeting = sequelize.define('Meeting', {
 	IdMeeting: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-	From: { type: Sequelize.DATE, allowNull: true },
-	To: { type: Sequelize.DATE, allowNull: true },
+	From: {
+		type: Sequelize.DATE, allowNull: true,
+		validate: {
+			isDate: {
+				msg: 'Pole powinno być prawidłową datą'
+			}
+		}
+	},
+	To: {
+		type: Sequelize.DATE, allowNull: true,
+		validate: {
+			isDate: {
+				msg: 'Pole powinno być prawidłową datą'
+			},
+			isGreaterThanFrom(value) {
+				if (value <= this.From) {
+					throw new Error('Data końcowa musi być późniejsza niż początkowa');
+				}
+			}
+		}
+	},
 	IdGroup: { type: Sequelize.INTEGER, allowNull: true },
 	IdRoom: { type: Sequelize.INTEGER, allowNull: true }
 }, {
