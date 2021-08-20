34827850303f9fb364b640e0f157112b53b563ef
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2021-08-20 22:49:02 +0200

    walidacja Meeting - sprawdzenie zajętości sali

--- model/sequelize/Meeting.js
+++ model/sequelize/Meeting.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../../config/sequelize/sequelize');
+const Op = Sequelize.Op;
 
 const Meeting = sequelize.define('Meeting', {
 	IdMeeting: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
@@ -8,6 +9,31 @@ const Meeting = sequelize.define('Meeting', {
 		validate: {
 			isDate: {
 				msg: 'Pole powinno być prawidłową datą'
+			},
+			isAvailable(value, next) {
+				const roomId = this.IdRoom;
+				const toDate = this.To;
+				Meeting.findAll({
+					where: {
+						[Op.or]: [
+							{
+								From: { [Op.lte]: value },
+								To: { [Op.gte]: value }
+							},
+							{
+								From: { [Op.gte]: value },
+								To: { [Op.lte]: toDate }
+							}
+						],
+						IdRoom: roomId
+					}
+				})
+					.then((meeting) => {
+						if (meeting.length != 0)
+							next(new Error(`Sala ${roomId} o godz. ${value} zajęta!`));
+						next();
+					})
+					.catch((onError) => console.log(onError));
 			}
 		}
 	},
@@ -19,8 +45,28 @@ const Meeting = sequelize.define('Meeting', {
 			},
 			isLessThanFrom(value) {
 				if (value <= this.From) {
-					throw new Error('Data końcowa musi być późniejsza niż początkowa');
+					throw new Error(`Data końcowa musi być późniejsza niż początkowa`);
 				}
+			},
+			isAvailable(value, next) {
+				const roomId = this.IdRoom
+				Meeting.findAll({
+					where: {
+						From: {
+							[Op.lte]: value
+						},
+						To: {
+							[Op.gte]: value
+						},
+						IdRoom: roomId
+					}
+				})
+					.then((meeting) => {
+						if (meeting.length != 0)
+							next(new Error(`Sala ${roomId} o godz. ${value} zajęta!`));
+						next();
+					})
+					.catch((onError) => console.log(onError));
 			}
 		}
 	},
