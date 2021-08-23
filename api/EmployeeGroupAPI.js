7b657d2ef7186faa0507eca72bb57cdbcb249505
Norbert Kamaszewski <norbert.kamaszewski@gmail.com>
2021-08-24 00:12:24 +0200

    poprawka w usuwaniu pracownika z grupy

--- api/EmployeeGroupAPI.js
+++ api/EmployeeGroupAPI.js
@@ -3,66 +3,65 @@ const EmployeeGroupRepository = require('../repository/sequelize/EmployeeGroupRe
 const GroupRepository = require('../repository/sequelize/GroupRepository');
 
 exports.getEmployeeGroups = (req, res, next) => {
-    EmployeeGroupRepository.getEmployeeGroups()
-        .then(emps => {
-            res.status(200).json(emps);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+  EmployeeGroupRepository.getEmployeeGroups()
+    .then((emps) => {
+      res.status(200).json(emps);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getEmployeeGroupById = (req, res, next) => {
-    const empGroupId = req.params.empGroupId;
-    EmployeeGroupRepository.getEmployeeGroupById(empGroupId)
-        .then(emp => {
-            if (!emp) {
-                res.status(404).json({
-                    message: 'EmployeeGroup with id: ' + empGroupId + ' not found'
-                })
-            } else {
-                res.status(200).json(emp);
-            }
-        });
+  const empGroupId = req.params.empGroupId;
+  EmployeeGroupRepository.getEmployeeGroupById(empGroupId).then((emp) => {
+    if (!emp) {
+      res.status(404).json({
+        message: 'EmployeeGroup with id: ' + empGroupId + ' not found',
+      });
+    } else {
+      res.status(200).json(emp);
+    }
+  });
 };
 
 exports.createEmployeeGroup = (req, res, next) => {
-    EmployeeGroupRepository.createEmployeeGroup(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+  EmployeeGroupRepository.createEmployeeGroup(req.body)
+    .then((newObj) => {
+      res.status(201).json(newObj);
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.updateEmployeeGroup = (req, res, next) => {
-    const empGroupId = req.params.empGroupId;
-    EmployeeGroupRepository.updateEmployeeGroup(empGroupId, req.body)
-        .then(result => {
-            res.status(200).json({ message: 'EmployeeGroup updated!', emp: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+  const empGroupId = req.params.empGroupId;
+  EmployeeGroupRepository.updateEmployeeGroup(empGroupId, req.body)
+    .then((result) => {
+      res.status(200).json({ message: 'EmployeeGroup updated!', emp: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.deleteEmployeeGroup = (req, res, next) => {
-    const empGroupId = req.params.empGroupId;
-    EmployeeGroupRepository.deleteEmployeeGroup(empGroupId)
-        .then(result => {
-            res.status(200).json({ message: 'Removed EmployeeGroup', emp: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+  const empGrpId = req.params.empGrpId;
+  EmployeeGroupRepository.deleteEmployeeGroup(empGrpId)
+    .then((result) => {
+      res.status(200).json({ message: 'Removed EmployeeGroup', emp: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
