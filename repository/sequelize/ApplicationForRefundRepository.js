6d8eba521d23a81915186bb17d11166d5d4c1c10
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2021-08-28 00:21:48 +0200

    dodane filtrowanie ApplicationForRefund

--- repository/sequelize/ApplicationForRefundRepository.js
+++ repository/sequelize/ApplicationForRefundRepository.js
@@ -1,6 +1,10 @@
 const ApplicationForReasons = require('../../model/sequelize/ApplicationForReasons');
 const ApplicationForRefund = require('../../model/sequelize/ApplicationForRefund');
 const ReasonForRefund = require('../../model/sequelize/ReasonForRefund');
+const ApplicationFor = require('../../model/sequelize/ApplicationFor');
+const Employee = require('../../model/sequelize/Employee');
+const Employment = require('../../model/sequelize/Employment');
+const Department = require('../../model/sequelize/Department');
 
 exports.getApplicationForRefunds = (params) => {
     const { iddepartment, iddivision, idstatus } = params
@@ -16,9 +20,44 @@ exports.getApplicationForRefunds = (params) => {
                 {
                     model: ReasonForRefund,
                     as: 'applicationForReasonsReasonForRefund',
+                    attributes: ['Name']
                 }
             ]
-        }]
+        },
+        {
+            model: ApplicationFor,
+            required: true,
+            as: 'applicationForRefundApplicationFor',
+            attributes: [],
+            include: [
+                {
+                    model: Employee,
+                    required: true,
+                    as: 'applicationForEmployee',
+                    attributes: [],
+                    include: [
+                        {
+                            model: Employment,
+                            required: true,
+                            as: 'employeeEmployment',
+                            attributes: [],
+                            where:
+                                depId ? { IdDepartment: depId, DateTo: null } : { DateTo: null },
+                            include: [
+                                {
+                                    model: Department,
+                                    required: true,
+                                    as: 'employmentsDepartment',
+                                    attributes: [],
+                                    where:
+                                        divId ? { IdDivision: divId } : {},
+                                }]
+                        }]
+                }
+            ]
+        }],
+        where:
+            statId ? { IdStatus: statId } : {}
     });
 };
 
