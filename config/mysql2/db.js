627a55e4b94715ef3d3f678d6b782c34aeac96b8
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2021-12-27 20:14:30 +0100

    heroku init

--- config/mysql2/db.js
+++ config/mysql2/db.js
@@ -1,8 +1,14 @@
 const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'pjatk_inz_root',
+//     password: 'root8TOOR',
+//     database: 'pjatk_inz_db'
+// });
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'pjatk_inz_root',
-    password: 'root8TOOR',
-    database: 'pjatk_inz_db'
+    host: 'ulsq0qqx999wqz84.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    user: 'z3cydyx3vshtdy4r',
+    password: 'qr2qbxyzocejj2yn',
+    database: 'j7dix0og2slovrp5'
 });
 module.exports = pool;
