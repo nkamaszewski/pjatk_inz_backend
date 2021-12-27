e0417888d6f5cd221d4258c9b7bd8b06efe552bb
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2021-12-27 21:37:28 +0100

    poprawka zapytania getAppFor

--- repository/mysql2/ApplicationForRepository.js
+++ repository/mysql2/ApplicationForRepository.js
@@ -10,34 +10,27 @@ exports.getApplicationFor = (params, ...userData) => {
     const divId = iddivision;
     const statId = idstatus;
     const [userId,userIdDepartment, userIdDivision,userIdRole] = userData;
-  
+
+    const whereText = `${statId ? "applicationfor.IdStatus = " + statId : 1} 
+            AND ${depId ? "employment.IdDepartment = " + depId : 1} 
+            AND ${divId ? "employment.IdDivision = " + divId : 1}  
+            AND ${userIdRole==Role.PRACOWNIK ? "employee.IdPerson = " + userId  : 
+            (userIdRole==Role.KIEROWNIK ?  "employment.IdDepartment = " + userIdDepartment : 
+            (userIdRole==Role.DYREKTOR ? "employment.IdDivision = " + userIdDivision : 1))} `;
 
     return db.promise().query(`
     SELECT IdApplicationFor, DateOfSubmission, status.name as Status, topic.topic as Nazwa, 'Kurs' as Rodzaj 
-    FROM pjatk_inz_db.applicationfor INNER JOIN status ON applicationfor.IdStatus=status.IdStatus INNER JOIN education ON applicationfor.IdEducation=education.IdEducation INNER JOIN training ON education.IdEducation=training.IdEducation INNER JOIN topic ON training.IdTopic=topic.IdTopic INNER JOIN employee ON applicationfor.IdPerson=employee.IdPerson INNER JOIN employment ON employee.IdPerson=employment.IdPerson INNER JOIN department ON employment.IdDepartment=department.IdDepartment 
-    WHERE  ${statId ? "applicationfor.IdStatus = " + statId : 1} 
-    AND ${depId ? "employment.IdDepartment = " + depId : 1} 
-    AND ${divId ? "employment.IdDivision = " + divId : 1}   
-    AND ${userIdRole==Role.PRACOWNIK ? "employee.IdPerson = " + userId  : 
-    (userIdRole==Role.KIEROWNIK ?  "employment.IdDepartment = " + userIdDepartment : 
-    (userIdRole==Role.DYREKTOR ? "employment.IdDivision = " + userIdDivision : 1))}
+    FROM applicationfor INNER JOIN status ON applicationfor.IdStatus=status.IdStatus INNER JOIN education ON applicationfor.IdEducation=education.IdEducation INNER JOIN training ON education.IdEducation=training.IdEducation INNER JOIN topic ON training.IdTopic=topic.IdTopic INNER JOIN employee ON applicationfor.IdPerson=employee.IdPerson INNER JOIN employment ON employee.IdPerson=employment.IdPerson INNER JOIN department ON employment.IdDepartment=department.IdDepartment 
+    WHERE  ${whereText}
+    
     UNION ALL 
-    SELECT IdApplicationFor, DateOfSubmission, status.name, study.FieldOfStudy as Nazwa, 'Studia' as Rodzaj FROM pjatk_inz_db.applicationfor INNER JOIN status ON applicationfor.IdStatus=status.IdStatus INNER JOIN education ON applicationfor.IdEducation=education.IdEducation INNER JOIN study ON education.IdEducation=study.IdEducation INNER JOIN employee ON applicationfor.IdPerson=employee.IdPerson INNER JOIN employment ON employee.IdPerson=employment.IdPerson INNER JOIN department ON employment.IdDepartment=department.IdDepartment 
-    WHERE  ${statId ? "applicationfor.IdStatus = " + statId : 1} 
-    AND ${depId ? "employment.IdDepartment = " + depId : 1} 
-    AND ${divId ? "employment.IdDivision = " + divId : 1}  
-    AND ${userIdRole==Role.PRACOWNIK ? "employee.IdPerson = " + userId  : 
-    (userIdRole==Role.KIEROWNIK ?  "employment.IdDepartment = " + userIdDepartment : 
-    (userIdRole==Role.DYREKTOR ? "employment.IdDivision = " + userIdDivision : 1))}
+    SELECT IdApplicationFor, DateOfSubmission, status.name, study.FieldOfStudy as Nazwa, 'Studia' as Rodzaj FROM applicationfor INNER JOIN status ON applicationfor.IdStatus=status.IdStatus INNER JOIN education ON applicationfor.IdEducation=education.IdEducation INNER JOIN study ON education.IdEducation=study.IdEducation INNER JOIN employee ON applicationfor.IdPerson=employee.IdPerson INNER JOIN employment ON employee.IdPerson=employment.IdPerson INNER JOIN department ON employment.IdDepartment=department.IdDepartment 
+    WHERE  ${whereText}
+   
     UNION ALL 
     SELECT IdApplicationFor, DateOfSubmission, status.name, otherEducation.Name as Nazwa, 'Inna edukacja' as Rodzaj 
-    FROM pjatk_inz_db.applicationfor INNER JOIN status ON applicationfor.IdStatus=status.IdStatus INNER JOIN education ON applicationfor.IdEducation=education.IdEducation INNER JOIN otherEducation ON education.IdEducation=otherEducation.IdEducation INNER JOIN employee ON applicationfor.IdPerson=employee.IdPerson INNER JOIN employment ON employee.IdPerson=employment.IdPerson INNER JOIN department ON employment.IdDepartment=department.IdDepartment 
-    WHERE  ${statId ? "applicationfor.IdStatus = " + statId : 1}
-    AND ${depId ? "employment.IdDepartment = " + depId : 1} 
-    AND ${divId ? "employment.IdDivision = " + divId : 1}
-    AND ${userIdRole==Role.PRACOWNIK ? "employee.IdPerson = " + userId  : 
-    (userIdRole==Role.KIEROWNIK ?  "employment.IdDepartment = " + userIdDepartment : 
-    (userIdRole==Role.DYREKTOR ? "employment.IdDivision = " + userIdDivision : 1))}
+    FROM applicationfor INNER JOIN status ON applicationfor.IdStatus=status.IdStatus INNER JOIN education ON applicationfor.IdEducation=education.IdEducation INNER JOIN otherEducation ON education.IdEducation=otherEducation.IdEducation INNER JOIN employee ON applicationfor.IdPerson=employee.IdPerson INNER JOIN employment ON employee.IdPerson=employment.IdPerson INNER JOIN department ON employment.IdDepartment=department.IdDepartment 
+    WHERE  ${whereText}
     `)
         .then((results, fields) => {
             return results[0];
