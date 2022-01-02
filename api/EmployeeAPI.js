7d493595a7edb73fc6ae45cee8d3da19b94c58e5
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-03 00:20:28 +0100

    poprawki walidacji i kontrolera employee

--- api/EmployeeAPI.js
+++ api/EmployeeAPI.js
@@ -1,197 +1,236 @@
-const EmployeeRepository = require('../repository/sequelize/EmployeeRepository');
-const Role = require('../model/Role')
+const EmployeeRepository = require("../repository/sequelize/EmployeeRepository");
+const Role = require("../model/Role");
 
 exports.getEmployees = (req, res, next) => {
-  EmployeeRepository.getEmployees()
-    .then((emps) => {
-      res.status(200).json(emps);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	EmployeeRepository.getEmployees()
+		.then((emps) => {
+			res.status(200).json(emps);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getEmployeeById = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getEmployeeById(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'Employee with id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getEmployeeById(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "Employee with id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.createEmployee = (req, res, next) => {
-  if (req.userIdRole != Role.ADMIN) {
-    res.status(403).json({
-        message: 'Brak uprawnień'
-    })
-}
-  EmployeeRepository.createEmployee(req.body)
-    .then((newObj) => {
-      res.status(201).json(newObj);
-    })
-    .catch((err) => {
-      console.log(err);
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	EmployeeRepository.createEmployee(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje w systemie pracownik z takim nr PESEL`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować danych pracownika`,
+				});
+			}
+		});
 };
 
 exports.updateEmployee = (req, res, next) => {
-  const empId = req.params.empId;
-  if (req.userIdRole != Role.ADMIN || req.userIdUser != empId) {
-    res.status(403).json({
-        message: 'Brak uprawnień'
-    }) 
-  } else {
-    console.log(req.userIdRole + ' - ' + empId)
-  }
-  
-  EmployeeRepository.updateEmployee(empId, req.body)
-    .then((result) => {
-      res.status(200).json({ message: 'Employee updated!', emp: result });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	const empId = req.params.empId;
+	if (req.userIdRole != Role.ADMIN || req.userIdUser != empId) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+
+	EmployeeRepository.updateEmployee(empId, req.body)
+		.then((result) => {
+			res.status(200).json({ message: "Employee updated!", emp: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Ta osoba jest już zdefiniowana jako pracownik`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować danych pracownika`,
+				});
+			}
+		});
 };
 
 exports.deleteEmployee = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.deleteEmployee(empId)
-    .then((result) => {
-      res.status(200).json({ message: 'Removed Employee', emp: result });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	const empId = req.params.empId;
+	EmployeeRepository.deleteEmployee(empId)
+		.then((result) => {
+			res.status(200).json({
+				message: "Usunięto pracownika",
+				emp: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message:
+						"Nie można usunąć pracownika, który posiada powiązane dane",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć szkoleniowca!",
+				});
+			}
+		});
 };
 
 exports.getQuestionnaireOffersByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getQuestionnaireOffersByEmpId(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'QuestionnaireOffer for id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getQuestionnaireOffersByEmpId(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "QuestionnaireOffer for id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.getApplicationsForByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getApplicationsForByEmpId(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'ApplicationFor for id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getApplicationsForByEmpId(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "ApplicationFor for id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.getAppStudiesByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getAppStudiesByEmpId(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'ApplicationFor for id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getAppStudiesByEmpId(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "ApplicationFor for id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.getAppTrainingsByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getAppTrainingsByEmpId(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'ApplicationFor for id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getAppTrainingsByEmpId(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "ApplicationFor for id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.getAppOthersByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getAppOthersByEmpId(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'ApplicationFor for id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getAppOthersByEmpId(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "ApplicationFor for id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.getParticipationsByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getParticipationsByEmpId(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'Participations for id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getParticipationsByEmpId(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "Participations for id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.getPartStudiesByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getPartStudiesByEmpId(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'Participations for id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getPartStudiesByEmpId(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "Participations for id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.getPartTrainingsByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getPartTrainingsByEmpId(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'Participations for id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getPartTrainingsByEmpId(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "Participations for id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.getPartOthersByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  EmployeeRepository.getPartOthersByEmpId(empId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'Participations for id: ' + empId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empId = req.params.empId;
+	EmployeeRepository.getPartOthersByEmpId(empId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "Participations for id: " + empId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
--- model/sequelize/Employee.js
+++ model/sequelize/Employee.js
@@ -1,48 +1,69 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
 const Employee = sequelize.define(
-  'Employee',
-  {
-    IdPerson: {
-      type: Sequelize.INTEGER,
-      autoIncrement: false,
-      allowNull: false,
-      primaryKey: true,
-    },
-    Pesel: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        notEmpty: {
-          msg: 'Pole jest wymagane',
-        },
-        len: {
-          args: [11, 11],
-          msg: 'Pole powinno zawierać 11 znaków',
-        },
-        isNumeric: {
-          msg: 'Pole powinno zawierać tylko cyfry',
-        },
-      },
-    },
-    Password: { type: Sequelize.STRING, allowNull: false },
-    IdRole: { type: Sequelize.INTEGER, allowNull: false, defaultValue: '1' },
-    IsActive: { type: Sequelize.BOOLEAN, allowNull: false },
-    OwnerAccount: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
-
-  },
-  {
-    timestamps: false,
-    tableName: 'Employee',
-    indexes: [
-      {
-        name: 'idx_employee_pesel',
-        unique: true,
-        fields: ['Pesel'],
-      },
-    ],
-  }
+	"Employee",
+	{
+		IdPerson: {
+			type: Sequelize.INTEGER,
+			autoIncrement: false,
+			allowNull: false,
+			primaryKey: true,
+		},
+		Pesel: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notEmpty: {
+					msg: "Należy podać nr PESEL",
+				},
+				notNull: {
+					msg: "Należy podać nr PESEL",
+				},
+				len: {
+					args: [11, 11],
+					msg: "Nr PESEL powinien zawierać 11 znaków",
+				},
+				isNumeric: {
+					msg: "Nr PESEL powinien zawierać tylko cyfry",
+				},
+			},
+		},
+		Password: { type: Sequelize.STRING, allowNull: false },
+		IdRole: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			defaultValue: "1",
+			validate: {
+				notNull: {
+					msg: "Należy określić rolę",
+				},
+				notEmpty: {
+					msg: "Należy określić rolę",
+				},
+				isInt: {
+					msg: "Należy określić rolę",
+				},
+			},
+		},
+		IsActive: { type: Sequelize.BOOLEAN, allowNull: false },
+		OwnerAccount: {
+			type: Sequelize.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "Employee",
+		indexes: [
+			{
+				name: "idx_employee_pesel",
+				unique: true,
+				fields: ["Pesel"],
+			},
+		],
+	}
 );
 module.exports = Employee;
