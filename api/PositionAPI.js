6068030aeae03a1a7a8b773eedacae8a29a37481
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-02 21:04:46 +0100

    poprawa walidacji i kontrolera position

--- api/PositionAPI.js
+++ api/PositionAPI.js
@@ -1,82 +1,123 @@
-const PositionRepository = require('../repository/sequelize/PositionRepository');
-const Role = require('../model/Role')
+const PositionRepository = require("../repository/sequelize/PositionRepository");
+const Role = require("../model/Role");
 
 exports.getPositions = (req, res, next) => {
-    PositionRepository.getPositions()
-        .then(poss => {
-            res.status(200).json(poss);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	PositionRepository.getPositions()
+		.then((poss) => {
+			res.status(200).json(poss);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getPositionById = (req, res, next) => {
-    const posId = req.params.posId;
-    PositionRepository.getPositionById(posId)
-        .then(pos => {
-            if (!pos) {
-                res.status(404).json({
-                    message: 'Position with id: ' + posId + ' not found'
-                })
-            } else {
-                res.status(200).json(pos);
-            }
-        });
+	const posId = req.params.posId;
+	PositionRepository.getPositionById(posId).then((pos) => {
+		if (!pos) {
+			res.status(404).json({
+				message: "Position with id: " + posId + " not found",
+			});
+		} else {
+			res.status(200).json(pos);
+		}
+	});
 };
 
 exports.createPosition = (req, res, next) => {
-    if (req.userIdRole != Role.ADMIN) {
-        res.status(403).json({
-            message: 'Brak uprawnień'
-        })
-    }
-    PositionRepository.createPosition(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	PositionRepository.createPosition(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje stanowisko o takiej nazwie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się utworzyć stanowiska`,
+				});
+			}
+		});
 };
 
 exports.updatePosition = (req, res, next) => {
-    if (req.userIdRole != Role.ADMIN) {
-        res.status(403).json({
-            message: 'Brak uprawnień'
-        })
-    }
-    const posId = req.params.posId;
-    PositionRepository.updatePosition(posId, req.body)
-        .then(result => {
-            res.status(200).json({ message: 'Position updated!', pos: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	const posId = req.params.posId;
+	PositionRepository.updatePosition(posId, req.body)
+		.then((result) => {
+			res.status(200).json({ message: "Position updated!", pos: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje stanowisko o takiej nazwie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować stanowiska`,
+				});
+			}
+		});
 };
 
 exports.deletePosition = (req, res, next) => {
-    if (req.userIdRole != Role.ADMIN) {
-        res.status(403).json({
-            message: 'Brak uprawnień'
-        })
-    }
-    const posId = req.params.posId;
-    PositionRepository.deletePosition(posId)
-        .then(result => {
-            res.status(200).json({ message: 'Removed Position', pos: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	const posId = req.params.posId;
+	PositionRepository.deletePosition(posId)
+		.then((result) => {
+			res.status(200).json({
+				message: "Stanowisko usunięto",
+				pos: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message:
+						"Nie można usunąć stanowiska, które jest przypisane zatrudnionym",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć stanowiska!",
+				});
+			}
+		});
 };
--- model/sequelize/Position.js
+++ model/sequelize/Position.js
@@ -1,22 +1,36 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const Position = sequelize.define('Position', {
-    IdPosition: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-    Name: {
-        type: Sequelize.STRING, allowNull: false, unique: true,
-        validate: {
-            notEmpty: {
-                msg: "Pole jest wymagane"
-            },
-            len: {
-                args: [2, 255],
-                msg: "Pole powinno zawierać od 5 do 60 znaków"
-            }
-        }
-    }
-}, {
-    timestamps: false,
-    tableName: 'Position'
-});
+const Position = sequelize.define(
+	"Position",
+	{
+		IdPosition: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		Name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notEmpty: {
+					msg: "Należy podać nazwę stanowiska",
+				},
+				notNull: {
+					msg: "Należy podać nazwę stanowiska",
+				},
+				len: {
+					args: [2, 255],
+					msg: "Nazwa powinno zawierać od 5 do 60 znaków",
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "Position",
+	}
+);
 module.exports = Position;
