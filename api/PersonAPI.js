f5591ed54ad5c5fc961ea2910364baa660b75b53
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-02 22:33:10 +0100

    poprawki walidacji i kontrolera person

--- api/PersonAPI.js
+++ api/PersonAPI.js
@@ -1,86 +1,119 @@
-const PersonRepository = require('../repository/sequelize/PersonRepository');
-const Role = require('../model/Role');
+const PersonRepository = require("../repository/sequelize/PersonRepository");
+const Role = require("../model/Role");
 
 exports.getPersons = (req, res, next) => {
-  PersonRepository.getPersons()
-    .then((pers) => {
-      res.status(200).json(pers);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	PersonRepository.getPersons()
+		.then((pers) => {
+			res.status(200).json(pers);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getPersonById = (req, res, next) => {
-  const perId = req.params.perId;
-  PersonRepository.getPersonById(perId).then((per) => {
-    if (!per) {
-      res.status(404).json({
-        message: 'Person with id: ' + perId + ' not found',
-      });
-    } else {
-      res.status(200).json(per);
-    }
-  });
+	const perId = req.params.perId;
+	PersonRepository.getPersonById(perId).then((per) => {
+		if (!per) {
+			res.status(404).json({
+				message: "Person with id: " + perId + " not found",
+			});
+		} else {
+			res.status(200).json(per);
+		}
+	});
 };
 
 exports.createPerson = (req, res, next) => {
-  if (req.userIdRole != Role.ADMIN) {
-    res.status(403).json({
-        message: 'Brak uprawnień'
-    })
-}
-  PersonRepository.createPerson(req.body)
-    .then((newObj) => {
-      res.status(201).json(newObj);
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	PersonRepository.createPerson(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje osoba o takim adresie email`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się dodać osoby`,
+				});
+			}
+		});
 };
 
 exports.updatePerson = (req, res, next) => {
-  const perId = req.params.perId;
+	const perId = req.params.perId;
 
-    if ((req.userId != perId) && (req.userIdRole != Role.ADMIN) ) {
-      console.log("Brak uprawnień");
-        res.status(403).json({
-            message: 'Brak uprawnień'
-        })
-    } else {
-
-  PersonRepository.updatePerson(perId, req.body)
-    .then(async () => {
-      const Person = await PersonRepository.getPersonById(perId);
-      res.status(200).json({ Person });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
-  };
+	if (req.userId != perId && req.userIdRole != Role.ADMIN) {
+		console.log("Brak uprawnień");
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	} else {
+		PersonRepository.updatePerson(perId, req.body)
+			.then(async () => {
+				const Person = await PersonRepository.getPersonById(perId);
+				res.status(200).json({ Person });
+			})
+			.catch((err) => {
+				if (err.name === "SequelizeUniqueConstraintError") {
+					res.status(403).json({
+						message: `Istnieje osoba o takim adresie email`,
+					});
+				} else if (err.name === "SequelizeValidationError") {
+					let message = "";
+					for (let m of err.errors) {
+						message += m.message + "\n";
+					}
+					res.status(403).json({
+						message,
+					});
+				} else {
+					if (!err.statusCode) {
+						err.statusCode = 500;
+					}
+					res.status(403).json({
+						message: `Nie udało się zaktualizować osoby`,
+					});
+				}
+			});
+	}
 };
 
 exports.deletePerson = (req, res, next) => {
-  if (req.userIdRole != Role.ADMIN) {
-    res.status(403).json({
-        message: 'Brak uprawnień'
-    })
-}
-  const perId = req.params.perId;
-  PersonRepository.deletePerson(perId)
-    .then((result) => {
-      res.status(200).json({ message: 'Removed Person', per: result });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	const perId = req.params.perId;
+	PersonRepository.deletePerson(perId)
+		.then((result) => {
+			res.status(200).json({ message: "Osobę usunięto", per: result });
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			res.status(403).json({
+				message: "Nie udało się usunąć osoby!",
+			});
+		});
 };
--- model/sequelize/Person.js
+++ model/sequelize/Person.js
@@ -1,64 +1,95 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const Person = sequelize.define('Person', {
-    IdPerson: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-    FirstName: {
-        type: Sequelize.STRING, allowNull: false,
-        validate: {
-            notEmpty: {
-                msg: "Pole jest wymagane"
-            },
-            len: {
-                args: [2, 255],
-                msg: "Pole powinno zawierać od 2 do 255 znaków"
-            }
-        }
-    },
-    LastName: {
-        type: Sequelize.STRING, allowNull: false,
-        validate: {
-            notEmpty: {
-                msg: "Pole jest wymagane"
-            },
-            len: {
-                args: [2, 255],
-                msg: "Pole powinno zawierać od 2 do 255 znaków"
-            }
-        }
-    },
-    Email: {
-        type: Sequelize.STRING, allowNull: false, unique: true,
-        validate: {
-            notEmpty: {
-                msg: "Pole jest wymagane"
-            },
-            len: {
-                args: [4, 255],
-                msg: "Pole powinno zawierać od 4 do 255 znaków"
-            },
-            isEmail: {
-                msg: 'Pole powinno zawierać prawidłowy adres email'
-            }
-        }
-    },
-    Phone: { type: Sequelize.STRING, allowNull: false }
-},
-    {
-        timestamps: false,
-        tableName: 'Person',
-        indexes: [
-            {
-                name: 'idx_person_email',
-                unique: true,
-                fields: ['Email']
-            },
-            {
-                name: 'idx_person_LastName_FirstName',
-                fields: ['LastName', 'FirstName']
-            }
-        ]
-    }
+const Person = sequelize.define(
+	"Person",
+	{
+		IdPerson: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		FirstName: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy wpisać imię",
+				},
+				notNull: {
+					msg: "Należy wpisać imię",
+				},
+				len: {
+					args: [2, 255],
+					msg: "Pole powinno zawierać od 2 do 255 znaków",
+				},
+			},
+		},
+		LastName: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy wpisać nazwisko",
+				},
+				notNull: {
+					msg: "Należy wpisać nazwisko",
+				},
+				len: {
+					args: [2, 255],
+					msg: "Pole powinno zawierać od 2 do 255 znaków",
+				},
+			},
+		},
+		Email: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notEmpty: {
+					msg: "'Należy podać prawidłowy adres email",
+				},
+				notNull: {
+					msg: "'Należy podać prawidłowy adres email",
+				},
+				isEmail: {
+					msg: "Należy podać prawidłowy adres email",
+				},
+				len: {
+					args: [4, 255],
+					msg: "Pole powinno zawierać od 4 do 255 znaków",
+				},
+			},
+		},
+		Phone: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy wpisać numer telefonu",
+				},
+				notNull: {
+					msg: "Należy wpisać numer telefonu",
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "Person",
+		indexes: [
+			{
+				name: "idx_person_email",
+				unique: true,
+				fields: ["Email"],
+			},
+			{
+				name: "idx_person_LastName_FirstName",
+				fields: ["LastName", "FirstName"],
+			},
+		],
+	}
 );
 
 module.exports = Person;
