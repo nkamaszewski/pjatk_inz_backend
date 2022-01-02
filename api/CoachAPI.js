9c3aefe1796a52a77f974a22cebf16fb90860622
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-02 23:57:04 +0100

    poprawki walidacji i kontrolera coach

--- api/CoachAPI.js
+++ api/CoachAPI.js
@@ -1,66 +1,107 @@
-const CoachRepository = require('../repository/sequelize/CoachRepository');
+const CoachRepository = require("../repository/sequelize/CoachRepository");
 
 exports.getCoachs = (req, res, next) => {
-    CoachRepository.getCoachs()
-        .then(coachs => {
-            res.status(200).json(coachs);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	CoachRepository.getCoachs()
+		.then((coachs) => {
+			res.status(200).json(coachs);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getCoachById = (req, res, next) => {
-    const coachId = req.params.coachId;
-    CoachRepository.getCoachById(coachId)
-        .then(coach => {
-            if (!coach) {
-                res.status(404).json({
-                    message: 'Coach with id: ' + coachId + ' not found'
-                })
-            } else {
-                res.status(200).json(coach);
-            }
-        });
+	const coachId = req.params.coachId;
+	CoachRepository.getCoachById(coachId).then((coach) => {
+		if (!coach) {
+			res.status(404).json({
+				message: "Coach with id: " + coachId + " not found",
+			});
+		} else {
+			res.status(200).json(coach);
+		}
+	});
 };
 
 exports.createCoach = (req, res, next) => {
-    CoachRepository.createCoach(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	CoachRepository.createCoach(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Ta osoba jest już zdefiniowana jako szkoleniowiec`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować szkoleniowca`,
+				});
+			}
+		});
 };
 
 exports.updateCoach = (req, res, next) => {
-    const coachId = req.params.coachId;
-    CoachRepository.updateCoach(coachId, req.body)
-        .then(result => {
-            res.status(200).json({ message: 'Coach updated!', coach: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const coachId = req.params.coachId;
+	CoachRepository.updateCoach(coachId, req.body)
+		.then((result) => {
+			res.status(200).json({ message: "Coach updated!", coach: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Ta osoba jest już zdefiniowana jako szkoleniowiec`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować szkoleniowca`,
+				});
+			}
+		});
 };
 
 exports.deleteCoach = (req, res, next) => {
-    const coachId = req.params.coachId;
-    CoachRepository.deleteCoach(coachId)
-        .then(result => {
-            res.status(200).json({ message: 'Removed Coach', coach: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const coachId = req.params.coachId;
+	CoachRepository.deleteCoach(coachId)
+		.then((result) => {
+			res.status(200).json({
+				message: "Usunięto szkoleniowca",
+				coach: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message:
+						"Nie można usunąć szkoleniowca, który ma przypisane szkolenia",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć szkoleniowca!",
+				});
+			}
+		});
 };
--- model/sequelize/Coach.js
+++ model/sequelize/Coach.js
@@ -1,22 +1,35 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const Coach = sequelize.define('Coach', {
-    IdPerson: { type: Sequelize.INTEGER, autoIncrement: false, allowNull: false, primaryKey: true },
-    JobTitle: {
-        type: Sequelize.STRING, allowNull: false,
-        validate: {
-            notEmpty: {
-                msg: "Pole jest wymagane"
-            },
-            len: {
-                args: [2, 255],
-                msg: "Pole powinno zawierać od 2 do 255 znaków"
-            }
-        }
-    }
-}, {
-    timestamps: false,
-    tableName: 'Coach'
-});
+const Coach = sequelize.define(
+	"Coach",
+	{
+		IdPerson: {
+			type: Sequelize.INTEGER,
+			autoIncrement: false,
+			allowNull: false,
+			primaryKey: true,
+		},
+		JobTitle: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy podać tytuł lub uprawnienia szkoleniowca",
+				},
+				notNull: {
+					msg: "Należy podać tytuł lub uprawnienia szkoleniowca",
+				},
+				len: {
+					args: [2, 255],
+					msg: "Pole powinno zawierać od 2 do 255 znaków",
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "Coach",
+	}
+);
 module.exports = Coach;
