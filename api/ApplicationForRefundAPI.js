01a6fbed60f8116f3d9b97720ad93e05ef9ade85
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-02 16:39:39 +0100

    poprawki walidacji applicationForRefund

--- api/ApplicationForRefundAPI.js
+++ api/ApplicationForRefundAPI.js
@@ -1,68 +1,85 @@
-const ApplicationForRefundRepository = require('../repository/sequelize/ApplicationForRefundRepository');
+const ApplicationForRefundRepository = require("../repository/sequelize/ApplicationForRefundRepository");
 
 exports.getApplicationForRefunds = (req, res, next) => {
-    const params = req.query
+	const params = req.query;
 
-    ApplicationForRefundRepository.getApplicationForRefunds(params)
-        .then(appForRefs => {
-            res.status(200).json(appForRefs);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	ApplicationForRefundRepository.getApplicationForRefunds(params)
+		.then((appForRefs) => {
+			res.status(200).json(appForRefs);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getApplicationForRefundById = (req, res, next) => {
-    const appForRefundId = req.params.appForRefundId;
-    ApplicationForRefundRepository.getApplicationForRefundById(appForRefundId)
-        .then(appForRef => {
-            if (!appForRef) {
-                res.status(404).json({
-                    message: 'Application for refund with id: ' + appForRefundId + ' not found'
-                })
-            } else {
-                res.status(200).json(appForRef);
-            }
-        });
+	const appForRefundId = req.params.appForRefundId;
+	ApplicationForRefundRepository.getApplicationForRefundById(
+		appForRefundId
+	).then((appForRef) => {
+		if (!appForRef) {
+			res.status(404).json({
+				message:
+					"Application for refund with id: " +
+					appForRefundId +
+					" not found",
+			});
+		} else {
+			res.status(200).json(appForRef);
+		}
+	});
 };
 
 exports.createApplicationForRefund = (req, res, next) => {
-    ApplicationForRefundRepository.createApplicationForRefund(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	ApplicationForRefundRepository.createApplicationForRefund(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeValidationError") {
+				res.status(403).json({ message: err.errors[0].message });
+			} else if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
 
 exports.updateApplicationForRefund = (req, res, next) => {
-    const appForRefundId = req.params.appForRefundId;
-    ApplicationForRefundRepository.updateApplicationForRefund(appForRefundId, req.body)
-        .then(result => {
-            res.status(200).json({ message: 'Application for refund updated!', appForRef: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const appForRefundId = req.params.appForRefundId;
+	ApplicationForRefundRepository.updateApplicationForRefund(
+		appForRefundId,
+		req.body
+	)
+		.then((result) => {
+			res.status(200).json({
+				message: "Zaktualizowano wniosek dodatkowy!",
+				appForRef: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeValidationError") {
+				res.status(403).json({ message: err.errors[0].message });
+			} else if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
 
 exports.deleteApplicationForRefund = (req, res, next) => {
-    const appForRefundId = req.params.appForRefundId;
-    ApplicationForRefundRepository.deleteApplicationForRefund(appForRefundId)
-        .then(result => {
-            res.status(200).json({ message: 'Removed application for refund', appForRefundId: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const appForRefundId = req.params.appForRefundId;
+	ApplicationForRefundRepository.deleteApplicationForRefund(appForRefundId)
+		.then((result) => {
+			res.status(200).json({
+				message: "Usunięto wniosek dodatkowy",
+				appForRefundId: result,
+			});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
--- model/sequelize/ApplicationForRefund.js
+++ model/sequelize/ApplicationForRefund.js
@@ -3,8 +3,25 @@ const sequelize = require('../../config/sequelize/sequelize');
 
 const ApplicationForRefund = sequelize.define('ApplicationForRefund', {
     IdApplicationForRefund: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-    IdApplicationFor: { type: Sequelize.INTEGER, allowNull: false },
-    IdStatus: { type: Sequelize.INTEGER, allowNull: false },
+    IdApplicationFor: { type: Sequelize.INTEGER, allowNull: false,
+        validate: {
+            notNull: {
+                msg: "Należy wybrać powiązany wniosek szkoleniowy!"
+            },
+            isInt: {
+                msg: "Należy wybrać powiązany wniosek szkoleniowy!"
+            }
+        },
+     },
+    IdStatus: { type: Sequelize.INTEGER, allowNull: false,
+        validate: {
+            notNull: {
+                msg: "Należy okerślić status wniosku!"
+            },
+            isInt: {
+                msg: "Należy okerślić status wniosku"
+            }
+        }, },
     DateOfSubmission: { type: Sequelize.DATE, allowNull: false }
 }, {
     timestamps: false,
