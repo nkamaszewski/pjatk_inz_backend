d11a571d5f4041be743f7e726083aafed5d757a3
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-03 23:57:51 +0100

    poprawki walidacji i obsł. błędów training

--- api/TrainingAPI.js
+++ api/TrainingAPI.js
@@ -1,82 +1,117 @@
-const TrainingRepository = require('../repository/sequelize/TrainingRepository');
+const TrainingRepository = require("../repository/sequelize/TrainingRepository");
 
 exports.getTrainings = (req, res, next) => {
-    const params = req.query
+	const params = req.query;
 
-    TrainingRepository.getTrainings(params)
-        .then(trns => {
-            res.status(200).json(trns);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	TrainingRepository.getTrainings(params)
+		.then((trns) => {
+			res.status(200).json(trns);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getTrainingById = (req, res, next) => {
-    const eduId = req.params.eduId;
-    TrainingRepository.getTrainingById(eduId)
-        .then(trn => {
-            if (!trn) {
-                res.status(404).json({
-                    message: 'Training with id: ' + eduId + ' not found'
-                })
-            } else {
-                res.status(200).json(trn);
-            }
-        });
+	const eduId = req.params.eduId;
+	TrainingRepository.getTrainingById(eduId).then((trn) => {
+		if (!trn) {
+			res.status(404).json({
+				message: "Training with id: " + eduId + " not found",
+			});
+		} else {
+			res.status(200).json(trn);
+		}
+	});
 };
 
 exports.createTraining = (req, res, next) => {
-    TrainingRepository.createTraining(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	TrainingRepository.createTraining(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować firmy`,
+				});
+			}
+		});
 };
 
 exports.updateTraining = (req, res, next) => {
-    const eduId = req.params.eduId;
-    TrainingRepository.updateTraining(eduId, req.body)
-        .then(result => {
-            res.status(200).json({ message: 'Training updated!', trn: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const eduId = req.params.eduId;
+	TrainingRepository.updateTraining(eduId, req.body)
+		.then((result) => {
+			res.status(200).json({
+				message: "Szkolenie zaktualizowane!",
+				trn: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować szkolenia`,
+				});
+			}
+		});
 };
 
 exports.deleteTraining = (req, res, next) => {
-    const eduId = req.params.eduId;
-    TrainingRepository.deleteTraining(eduId)
-        .then(result => {
-            res.status(200).json({ message: 'Removed Training', trn: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const eduId = req.params.eduId;
+	TrainingRepository.deleteTraining(eduId)
+		.then((result) => {
+			res.status(200).json({
+				message: "Usunięto szkolenie",
+				trn: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message:
+						"Nie można usunąć szkolenia, które posiada przypisane grupy",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć szkolenia!",
+				});
+			}
+		});
 };
 
 exports.getTrainingByInternal = (req, res, next) => {
-    const int = req.params.int;
-    TrainingRepository.getTrainingByInternal(int)
-        .then(trn => {
-            if (!trn) {
-                res.status(404).json({
-                    message: 'Training with Internal: ' + int + ' not found'
-                })
-            } else {
-                res.status(200).json(trn);
-            }
-        });
+	const int = req.params.int;
+	TrainingRepository.getTrainingByInternal(int).then((trn) => {
+		if (!trn) {
+			res.status(404).json({
+				message: "Training with Internal: " + int + " not found",
+			});
+		} else {
+			res.status(200).json(trn);
+		}
+	});
 };
--- model/sequelize/Training.js
+++ model/sequelize/Training.js
@@ -1,49 +1,107 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const Training = sequelize.define('Training', {
-	IdEducation: { type: Sequelize.INTEGER, autoIncrement: false, allowNull: false, primaryKey: true },
-	IdTopic: { type: Sequelize.INTEGER, allowNull: false },
-	IdCompany: { type: Sequelize.INTEGER, allowNull: false },
-	IdPerson: { type: Sequelize.INTEGER, allowNull: false },
-	Internal: { type: Sequelize.BOOLEAN, allowNull: true, defaultValue: false },
-	DateFrom: {
-		type: Sequelize.DATE, allowNull: false,
-		validate: {
-			isDate: {
-				msg: 'Pole powinno być prawidłową datą'
-			}
-		}
+const Training = sequelize.define(
+	"Training",
+	{
+		IdEducation: {
+			type: Sequelize.INTEGER,
+			autoIncrement: false,
+			allowNull: false,
+			primaryKey: true,
+		},
+		IdTopic: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy wybrać temat szkolenia",
+				},
+				isInt: {
+					msg: "Należy wybrać temat szkolenia",
+				},
+			},
+		},
+		IdCompany: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy wybrać organizatora szkolenia",
+				},
+				isInt: {
+					msg: "Należy wybrać organizatora szkolenia",
+				},
+			},
+		},
+		IdPerson: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy wybrać prowadzącego szkolenie",
+				},
+				isInt: {
+					msg: "Należy wybrać prowadzącego szkolenie",
+				},
+			},
+		},
+		Internal: {
+			type: Sequelize.BOOLEAN,
+			allowNull: true,
+			defaultValue: false,
+		},
+		DateFrom: {
+			type: Sequelize.DATE,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Data początkowa nie może być pusta",
+				},
+				isDate: {
+					msg: "Pole powinno być prawidłową datą",
+				},
+			},
+		},
+		DateTo: {
+			type: Sequelize.DATE,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Data końcowa nie może być pusta",
+				},
+				isDate: {
+					msg: "Pole powinno być prawidłową datą",
+				},
+				isLessThanFrom(value) {
+					if (value < this.DateFrom) {
+						console.log("To " + value);
+						console.log("To " + this.DateFrom);
+						throw new Error(
+							`Data końcowa musi być późniejsza niż początkowa`
+						);
+					}
+				},
+			},
+		},
 	},
-	DateTo: {
-		type: Sequelize.DATE, allowNull: false,
-		validate: {
-			isDate: {
-				msg: 'Pole powinno być prawidłową datą'
+	{
+		timestamps: false,
+		tableName: "Training",
+		indexes: [
+			{
+				name: "idx_training_idTopic",
+				fields: ["IdTopic"],
 			},
-			isLessThanFrom(value) {
-				if (value <= this.DateFrom) {
-					throw new Error(`Data końcowa musi być późniejsza niż początkowa`);
-				}
+			{
+				name: "idx_training_idCompany",
+				fields: ["IdCompany"],
 			},
-		}
+			{
+				name: "idx_training_idPerson",
+				fields: ["IdPerson"],
+			},
+		],
 	}
-}, {
-	timestamps: false,
-	tableName: 'Training',
-	indexes: [
-		{
-			name: 'idx_training_idTopic',
-			fields: ['IdTopic']
-		},
-		{
-			name: 'idx_training_idCompany',
-			fields: ['IdCompany']
-		},
-		{
-			name: 'idx_training_idPerson',
-			fields: ['IdPerson']
-		}
-	]
-});
+);
 module.exports = Training;
