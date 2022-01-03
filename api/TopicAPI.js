7aa9c4617374e706329b21a87bbbf9c207a87efb
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-03 21:48:30 +0100

    poprawki walidacji i kom. błęd. topic

--- api/TopicAPI.js
+++ api/TopicAPI.js
@@ -1,66 +1,120 @@
-const TopicRepository = require('../repository/sequelize/TopicRepository');
+const TopicRepository = require("../repository/sequelize/TopicRepository");
+const Role = require("../model/Role");
 
 exports.getTopics = (req, res, next) => {
-    TopicRepository.getTopics()
-        .then(tops => {
-            res.status(200).json(tops);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	TopicRepository.getTopics()
+		.then((tops) => {
+			res.status(200).json(tops);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getTopicById = (req, res, next) => {
-    const topId = req.params.topId;
-    TopicRepository.getTopicById(topId)
-        .then(top => {
-            if (!top) {
-                res.status(404).json({
-                    message: 'Topic with id: ' + topId + ' not found'
-                })
-            } else {
-                res.status(200).json(top);
-            }
-        });
+	const topId = req.params.topId;
+	TopicRepository.getTopicById(topId).then((top) => {
+		if (!top) {
+			res.status(404).json({
+				message: "Topic with id: " + topId + " not found",
+			});
+		} else {
+			res.status(200).json(top);
+		}
+	});
 };
 
 exports.createTopic = (req, res, next) => {
-    TopicRepository.createTopic(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	TopicRepository.createTopic(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje już taki temat`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się dodać tematu`,
+				});
+			}
+		});
 };
 
 exports.updateTopic = (req, res, next) => {
-    const topId = req.params.topId;
-    TopicRepository.updateTopic(topId, req.body)
-        .then(result => {
-            res.status(200).json({ message: 'Topic updated!', top: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	const topId = req.params.topId;
+	TopicRepository.updateTopic(topId, req.body)
+		.then((result) => {
+			res.status(200).json({ message: "Topic updated!", top: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje już taki temat`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować tematu`,
+				});
+			}
+		});
 };
 
 exports.deleteTopic = (req, res, next) => {
-    const topId = req.params.topId;
-    TopicRepository.deleteTopic(topId)
-        .then(result => {
-            res.status(200).json({ message: 'Removed Topic', top: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	const topId = req.params.topId;
+	TopicRepository.deleteTopic(topId)
+		.then((result) => {
+			res.status(200).json({ message: "Removed Topic", top: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message:
+						"Nie można usunąć tematu ze względu na przypisane szkolenia",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć tematu!",
+				});
+			}
+		});
 };
--- model/sequelize/Topic.js
+++ model/sequelize/Topic.js
@@ -1,22 +1,54 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const Topic = sequelize.define('Topic', {
-    IdTopic: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-    Topic: { type: Sequelize.STRING, allowNull: false },
-    IdSubject: { type: Sequelize.INTEGER, allowNull: false }
-}, {
-    timestamps: false,
-    tableName: 'Topic',
-    indexes: [
-        {
-            name: 'idx_topic_idSubiect',
-            fields: ['IdSubject']
-        },
-        {
-            name: 'idx_topic_topic',
-            fields: ['Topic']
-        }
-    ]
-});
+const Topic = sequelize.define(
+	"Topic",
+	{
+		IdTopic: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		Topic: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notNull: {
+					msg: "Należy podać temat szkolenia",
+				},
+				notEmpty: {
+					msg: "Należy podać temat szkolenia",
+				},
+			},
+		},
+		IdSubject: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy wybrać tematykę",
+				},
+				isInt: {
+					msg: "Należy wybrać tematykę",
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "Topic",
+		indexes: [
+			{
+				name: "idx_topic_idSubiect",
+				fields: ["IdSubject"],
+			},
+			{
+				name: "idx_topic_topic",
+				fields: ["Topic"],
+			},
+		],
+	}
+);
 module.exports = Topic;
