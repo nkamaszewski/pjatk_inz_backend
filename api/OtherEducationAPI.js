f7a9263d1177106c57faa4dd15d8615a6c455fc4
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-04 00:29:59 +0100

    poprawki walidacji i obsł. błędów otherEducation

--- api/OtherEducationAPI.js
+++ api/OtherEducationAPI.js
@@ -1,66 +1,103 @@
-const OtherEducationRepository = require('../repository/sequelize/OtherEducationRepository');
+const OtherEducationRepository = require("../repository/sequelize/OtherEducationRepository");
 
 exports.getOtherEducations = (req, res, next) => {
-    OtherEducationRepository.getOtherEducations()
-        .then(oEdus => {
-            res.status(200).json(oEdus);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	OtherEducationRepository.getOtherEducations()
+		.then((oEdus) => {
+			res.status(200).json(oEdus);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getOtherEducationById = (req, res, next) => {
-    const eduId = req.params.eduId;
-    OtherEducationRepository.getOtherEducationById(eduId)
-        .then(oEdu => {
-            if (!oEdu) {
-                res.status(404).json({
-                    message: 'OtherEducation with id: ' + eduId + ' not found'
-                })
-            } else {
-                res.status(200).json(oEdu);
-            }
-        });
+	const eduId = req.params.eduId;
+	OtherEducationRepository.getOtherEducationById(eduId).then((oEdu) => {
+		if (!oEdu) {
+			res.status(404).json({
+				message: "OtherEducation with id: " + eduId + " not found",
+			});
+		} else {
+			res.status(200).json(oEdu);
+		}
+	});
 };
 
 exports.createOtherEducation = (req, res, next) => {
-    OtherEducationRepository.createOtherEducation(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	OtherEducationRepository.createOtherEducation(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje szkolenie o takiej nazwie tego organizatora`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się dodać szkolenia`,
+				});
+			}
+		});
 };
 
 exports.updateOtherEducation = (req, res, next) => {
-    const eduId = req.params.eduId;
-    OtherEducationRepository.updateOtherEducation(eduId, req.body)
-        .then(result => {
-            res.status(200).json({ message: 'OtherEducation updated!', oEdu: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const eduId = req.params.eduId;
+	OtherEducationRepository.updateOtherEducation(eduId, req.body)
+		.then((result) => {
+			res.status(200).json({
+				message: "Szkolenie zaktualizowano!",
+				oEdu: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje szkolenie o takiej nazwie tego organizatora`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować szkolenia`,
+				});
+			}
+		});
 };
 
 exports.deleteOtherEducation = (req, res, next) => {
-    const eduId = req.params.eduId;
-    OtherEducationRepository.deleteOtherEducation(eduId)
-        .then(result => {
-            res.status(200).json({ message: 'Removed OtherEducation', oEdu: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const eduId = req.params.eduId;
+	OtherEducationRepository.deleteOtherEducation(eduId)
+		.then((result) => {
+			res.status(200).json({
+				message: "Usunięto szkolenie",
+				oEdu: result,
+			});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
--- model/sequelize/OtherEducation.js
+++ model/sequelize/OtherEducation.js
@@ -1,12 +1,39 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const OtherEducation = sequelize.define('OtherEducation', {
-	IdEducation: { type: Sequelize.INTEGER, autoIncrement: false, allowNull: false, primaryKey: true },
-	Name: { type: Sequelize.STRING, allowNull: false },
-	IdCompany: { type: Sequelize.INTEGER, allowNull: false }
-}, {
-	timestamps: false,
-	tableName: 'OtherEducation'
-});
+const OtherEducation = sequelize.define(
+	"OtherEducation",
+	{
+		IdEducation: {
+			type: Sequelize.INTEGER,
+			autoIncrement: false,
+			allowNull: false,
+			primaryKey: true,
+		},
+		Name: { type: Sequelize.STRING, allowNull: false },
+		IdCompany: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy podać organizatora szkolenia",
+				},
+				isInt: {
+					msg: "Należy podać organizatora szkolenia",
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "OtherEducation",
+		indexes: [
+			{
+				name: "idx_otherEducation",
+				unique: true,
+				fields: ["Name", "IdCompany"],
+			},
+		],
+	}
+);
 module.exports = OtherEducation;
