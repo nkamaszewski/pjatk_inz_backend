81e309460f520b3c88e810c00269075ee45725c1
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-03 19:34:17 +0100

    poprawki walidacji i obsługi błędów room

--- api/RoomAPI.js
+++ api/RoomAPI.js
@@ -1,88 +1,126 @@
-const RoomRepository = require('../repository/sequelize/RoomRepository');
-const Role = require('../model/Role')
+const RoomRepository = require("../repository/sequelize/RoomRepository");
+const Role = require("../model/Role");
 
 exports.getRooms = (req, res, next) => {
-    RoomRepository.getRooms()
-        .then(rooms => {
-            res.status(200).json(rooms);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	RoomRepository.getRooms()
+		.then((rooms) => {
+			res.status(200).json(rooms);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getRoomById = (req, res, next) => {
-    const roomId = req.params.roomId;
-    RoomRepository.getRoomById(roomId)
-        .then(room => {
-            if (!room) {
-                res.status(404).json({
-                    message: 'Room with id: ' + roomId + ' not found'
-                })
-            } else {
-                res.status(200).json(room);
-            }
-        });
+	const roomId = req.params.roomId;
+	RoomRepository.getRoomById(roomId).then((room) => {
+		if (!room) {
+			res.status(404).json({
+				message: "Room with id: " + roomId + " not found",
+			});
+		} else {
+			res.status(200).json(room);
+		}
+	});
 };
 
 exports.createRoom = (req, res, next) => {
-    if (req.userIdRole != Role.ADMIN) {
-        res.status(403).json({
-            message: 'Brak uprawnień'
-        })
-    }
-    RoomRepository.createRoom(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	RoomRepository.createRoom(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje sala o takiej nazwie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować sali`,
+				});
+			}
+		});
 };
 
 exports.updateRoom = (req, res, next) => {
-    if (req.userIdRole != Role.ADMIN) {
-        res.status(403).json({
-            message: 'Brak uprawnień'
-        })
-    }
-    const roomId = req.params.roomId;
-    RoomRepository.updateRoom(roomId, req.body)
-        .then(result => {
-            res.status(200).json({
-                message: 'Room updated!',
-                room: result
-            });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	const roomId = req.params.roomId;
+	RoomRepository.updateRoom(roomId, req.body)
+		.then((result) => {
+			res.status(200).json({
+				message: "Room updated!",
+				room: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje sala o takiej nazwie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować sali`,
+				});
+			}
+		});
 };
 
 exports.deleteRoom = (req, res, next) => {
-    if (req.userIdRole != Role.ADMIN) {
-        res.status(403).json({
-            message: 'Brak uprawnień'
-        })
-    }
-    const roomId = req.params.roomId;
-    RoomRepository.deleteRoom(roomId)
-        .then(result => {
-            res.status(200).json({
-                message: 'Removed Room',
-                room: result
-            });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	const roomId = req.params.roomId;
+	RoomRepository.deleteRoom(roomId)
+		.then((result) => {
+			res.status(200).json({
+				message: "Salę usunięto",
+				room: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message:
+						"Nie można usunąć sali ze względu na przypisane spotkania",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć sali!",
+				});
+			}
+		});
 };
--- model/sequelize/Room.js
+++ model/sequelize/Room.js
@@ -1,58 +1,92 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const Room = sequelize.define('Room', {
-	IdRoom: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-	Name: {
-		type: Sequelize.STRING, allowNull: false, unique: true,
-		validate: {
-			notEmpty: {
-				msg: "Pole jest wymagane"
+const Room = sequelize.define(
+	"Room",
+	{
+		IdRoom: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		Name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notEmpty: {
+					msg: "Pole jest wymagane",
+				},
+				len: {
+					args: [1, 20],
+					msg: "Pole powinno zawierać od 1 do 20 znaków",
+				},
 			},
-			len: {
-				args: [1, 20],
-				msg: "Pole powinno zawierać od 1 do 20 znaków"
-			}
-		}
-	},
-	Area: { type: Sequelize.INTEGER, allowNull: false },
-	CapacitySet1: {
-		type: Sequelize.INTEGER, allowNull: true,
-		validate: {
-			notEmpty: {
-				msg: "Pole jest wymagane"
+		},
+		Area: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Pole jest wymagane",
+				},
+				isNumeric: {
+					msg: "Pole powinno zawierać liczbę",
+				},
+				min: {
+					args: 2,
+					msg: "Minimalna powierzchnia sali to 10",
+				},
 			},
-			isNumeric: {
-				msg: 'Pole powinno zawierać liczbę'
-			}
-		}
+		},
+		CapacitySet1: {
+			type: Sequelize.INTEGER,
+			allowNull: true,
+			validate: {
+				notEmpty: {
+					msg: "Pole jest wymagane",
+				},
+				isNumeric: {
+					msg: "Pole powinno zawierać liczbę",
+				},
+				min: {
+					args: 2,
+					msg: "Minimalna pojemność sali to 2",
+				},
+			},
+		},
+		CapacitySet2: {
+			type: Sequelize.INTEGER,
+			allowNull: true,
+			validate: {
+				isNumeric: {
+					msg: "Pole powinno zawierać liczbę",
+				},
+			},
+		},
+		CapacitySet3: {
+			type: Sequelize.INTEGER,
+			allowNull: true,
+			validate: {
+				isNumeric: {
+					msg: "Pole powinno zawierać liczbę",
+				},
+			},
+		},
+		CapacitySet4: {
+			type: Sequelize.INTEGER,
+			allowNull: true,
+			validate: {
+				isNumeric: {
+					msg: "Pole powinno zawierać liczbę",
+				},
+			},
+		},
 	},
-	CapacitySet2: {
-		type: Sequelize.INTEGER, allowNull: true,
-		validate: {
-			isNumeric: {
-				msg: 'Pole powinno zawierać liczbę'
-			}
-		}
-	},
-	CapacitySet3: {
-		type: Sequelize.INTEGER, allowNull: true,
-		validate: {
-			isNumeric: {
-				msg: 'Pole powinno zawierać liczbę'
-			}
-		}
-	},
-	CapacitySet4: {
-		type: Sequelize.INTEGER, allowNull: true,
-		validate: {
-			isNumeric: {
-				msg: 'Pole powinno zawierać liczbę'
-			}
-		}
+	{
+		timestamps: false,
+		tableName: "Room",
 	}
-}, {
-	timestamps: false,
-	tableName: 'Room'
-});
+);
 module.exports = Room;
