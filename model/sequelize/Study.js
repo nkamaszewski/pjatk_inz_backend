6d32d2c99f5c1cf15af558bcef1a09f432fb354e
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-04 00:55:45 +0100

    dodane filtrowanie traing po subject

--- model/sequelize/Study.js
+++ model/sequelize/Study.js
@@ -1,23 +1,65 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
 const Study = sequelize.define(
-  'Study',
-  {
-    IdEducation: {
-      type: Sequelize.INTEGER,
-      autoIncrement: false,
-      allowNull: false,
-      primaryKey: true,
-    },
-    FieldOfStudy: { type: Sequelize.STRING, allowNull: false },
-    IdUniversity: { type: Sequelize.INTEGER, allowNull: false },
-    IdStudyMode: { type: Sequelize.INTEGER, allowNull: false },
-    IdGraduateDegree: { type: Sequelize.INTEGER, allowNull: false },
-  },
-  {
-    timestamps: false,
-    tableName: 'Study',
-  }
+	"Study",
+	{
+		IdEducation: {
+			type: Sequelize.INTEGER,
+			autoIncrement: false,
+			allowNull: false,
+			primaryKey: true,
+		},
+		FieldOfStudy: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			notNull: {
+				msg: "Należy podać kierunek studiów",
+			},
+			notEmpty: {
+				msg: "Należy podać kierunek studiów",
+			},
+		},
+		IdUniversity: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Wymagany jest wybór uczelni",
+				},
+				isInt: {
+					msg: "Wymagany jest wybór uczelni",
+				},
+			},
+		},
+		IdStudyMode: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Wymagany jest wybór trybu studiów",
+				},
+				isInt: {
+					msg: "Wymagany jest wybór trybu studiów",
+				},
+			},
+		},
+		IdGraduateDegree: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Wymagany jest stopnia studiów",
+				},
+				isInt: {
+					msg: "Wymagany jest stopnia studiów",
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "Study",
+	}
 );
 module.exports = Study;
--- repository/sequelize/TrainingRepository.js
+++ repository/sequelize/TrainingRepository.js
@@ -1,118 +1,116 @@
-const Education = require('../../model/sequelize/Education');
-const Coach = require('../../model/sequelize/Coach');
-const Person = require('../../model/sequelize/Person');
-const Topic = require('../../model/sequelize/Topic');
-const Company = require('../../model/sequelize/Company');
-const Training = require('../../model/sequelize/Training');
-const Sequelize = require('sequelize');
+const Education = require("../../model/sequelize/Education");
+const Coach = require("../../model/sequelize/Coach");
+const Person = require("../../model/sequelize/Person");
+const Topic = require("../../model/sequelize/Topic");
+const Company = require("../../model/sequelize/Company");
+const Training = require("../../model/sequelize/Training");
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
-
 exports.getTrainings = (params) => {
-  const { internal, active } = params
-  console.log(internal);
-  return Training.findAll({
-    attributes: ['Internal', 'DateFrom', 'DateTo', 'IdEducation'],
-    include: [
-      {
-        model: Education,
-        as: 'trainingEducation',
-      },
-      {
-        model: Company,
-        as: 'trainingCompany',
-      },
-      {
-        model: Topic,
-        as: 'trainingTopic',
-      },
-      {
-        model: Coach,
-        as: 'trainingCoach',
-        include: [
-          {
-            model: Person,
-            as: 'CoachPerson',
-          },
-        ],
-      },
-    ],
-    where: [
-      internal ? { Internal: internal } : {},    
-      active == 1 ? { DateTo: { [Op.gte]: new Date() } } : {}
-    ],
-    order: [
-      ['DateFrom', 'DESC']
-  ],
-  });
+	const { internal, active, subject } = params;
+	console.log(internal);
+	return Training.findAll({
+		attributes: ["Internal", "DateFrom", "DateTo", "IdEducation"],
+		include: [
+			{
+				model: Education,
+				as: "trainingEducation",
+			},
+			{
+				model: Company,
+				as: "trainingCompany",
+			},
+			{
+				model: Topic,
+				as: "trainingTopic",
+				where: [subject ? { IdSubject: subject } : {}],
+			},
+			{
+				model: Coach,
+				as: "trainingCoach",
+				include: [
+					{
+						model: Person,
+						as: "CoachPerson",
+					},
+				],
+			},
+		],
+		where: [
+			internal ? { Internal: internal } : {},
+			active == 1 ? { DateTo: { [Op.gte]: new Date() } } : {},
+		],
+		order: [["DateFrom", "DESC"]],
+	});
 };
 
 exports.createTraining = (newTrainingData) => {
-  return Training.create({
-    IdEducation: newTrainingData.IdEducation,
-    IdTopic: newTrainingData.IdTopic,
-    IdCompany: newTrainingData.IdCompany,
-    IdPerson: newTrainingData.IdPerson,
-    Internal: newTrainingData.Internal,
-    DateFrom: newTrainingData.DateFrom,
-    DateTo: newTrainingData.DateTo,
-  });
+	return Training.create({
+		IdEducation: newTrainingData.IdEducation,
+		IdTopic: newTrainingData.IdTopic,
+		IdCompany: newTrainingData.IdCompany,
+		IdPerson: newTrainingData.IdPerson,
+		Internal: newTrainingData.Internal,
+		DateFrom: newTrainingData.DateFrom,
+		DateTo: newTrainingData.DateTo,
+	});
 };
 
 exports.deleteTraining = (eduId) => {
-  return Training.destroy({
-    where: { IdEducation: eduId },
-  });
+	return Training.destroy({
+		where: { IdEducation: eduId },
+	});
 };
 
 exports.updateTraining = (eduId, data) => {
-  const name = data.Name;
-  const idDivision = data.IdDivision;
+	const name = data.Name;
+	const idDivision = data.IdDivision;
 
-  const IdEducation = data.IdEducation;
-  const IdTopic = data.IdTopic;
-  const IdCompany = data.IdCompany;
-  const IdPerson = data.IdPerson;
-  const Internal = data.Internal;
-  const DateFrom = data.DateFrom;
-  const DateTo = data.DateTo;
+	const IdEducation = data.IdEducation;
+	const IdTopic = data.IdTopic;
+	const IdCompany = data.IdCompany;
+	const IdPerson = data.IdPerson;
+	const Internal = data.Internal;
+	const DateFrom = data.DateFrom;
+	const DateTo = data.DateTo;
 
-  return Training.update(data, { where: { IdEducation: eduId } });
+	return Training.update(data, { where: { IdEducation: eduId } });
 };
 
 exports.getTrainingById = (eduId) => {
-  return Training.findByPk(eduId);
+	return Training.findByPk(eduId);
 };
 
 exports.getTrainingByInternal = (int) => {
-  return Training.findAll({
-    attributes: ['Internal', 'DateFrom', 'DateTo'],
-    include: [
-      {
-        model: Education,
-        as: 'trainingEducation',
-      },
-      {
-        model: Company,
-        as: 'trainingCompany',
-      },
-      {
-        model: Topic,
-        as: 'trainingTopic',
-      },
-      {
-        model: Coach,
-        as: 'trainingCoach',
-        include: [
-          {
-            model: Person,
-            as: 'CoachPerson',
-          },
-        ],
-      },
-    ],
-    where: {
-      Internal: int,
-    },
-  });
+	return Training.findAll({
+		attributes: ["Internal", "DateFrom", "DateTo"],
+		include: [
+			{
+				model: Education,
+				as: "trainingEducation",
+			},
+			{
+				model: Company,
+				as: "trainingCompany",
+			},
+			{
+				model: Topic,
+				as: "trainingTopic",
+			},
+			{
+				model: Coach,
+				as: "trainingCoach",
+				include: [
+					{
+						model: Person,
+						as: "CoachPerson",
+					},
+				],
+			},
+		],
+		where: {
+			Internal: int,
+		},
+	});
 };
