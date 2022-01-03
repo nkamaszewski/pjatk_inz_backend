c5bd0af05c9b39ddc09ce0df78d91f30de4d33f5
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-03 20:05:45 +0100

    poprawki walidacji i komunikatów błędów meeting

--- api/MeetingAPI.js
+++ api/MeetingAPI.js
@@ -1,75 +1,99 @@
-const MeetingRepository = require('../repository/sequelize/MeetingRepository');
-const { mapToMeetingList } = require('../mappers/mapToMeetingList');
+const MeetingRepository = require("../repository/sequelize/MeetingRepository");
+const { mapToMeetingList } = require("../mappers/mapToMeetingList");
 
 exports.getMeetings = (req, res, next) => {
-  const { query } = req;
-  MeetingRepository.getMeetings(query)
-    .then((meets) => {
-      const meetingsList = mapToMeetingList(meets);
-      res.status(200).json(meetingsList);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	const { query } = req;
+	MeetingRepository.getMeetings(query)
+		.then((meets) => {
+			const meetingsList = mapToMeetingList(meets);
+			res.status(200).json(meetingsList);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getMeetingById = (req, res, next) => {
-  const meetId = req.params.meetId;
+	const meetId = req.params.meetId;
 
-  MeetingRepository.getMeetingById(meetId).then((meet) => {
-    if (!meet) {
-      res.status(404).json({
-        message: 'Meeting with id: ' + meetId + ' not found',
-      });
-    } else {
-      res.status(200).json(meet);
-    }
-  });
+	MeetingRepository.getMeetingById(meetId).then((meet) => {
+		if (!meet) {
+			res.status(404).json({
+				message: "Meeting with id: " + meetId + " not found",
+			});
+		} else {
+			res.status(200).json(meet);
+		}
+	});
 };
 
 exports.createMeeting = (req, res, next) => {
-  MeetingRepository.createMeeting(req.body)
-    .then((newObj) => {
-      res.status(201).json(newObj);
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	MeetingRepository.createMeeting(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować spotkania`,
+				});
+			}
+		});
 };
 
 exports.updateMeeting = (req, res, next) => {
-  const meetId = req.params.meetId;
-  MeetingRepository.updateMeeting(meetId, req.body)
-    .then((result) => {
-      res.status(200).json({
-        message: 'Meeting updated!',
-        meet: result,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	const meetId = req.params.meetId;
+	MeetingRepository.updateMeeting(meetId, req.body)
+		.then((result) => {
+			res.status(200).json({
+				message: "Spotkanie zaktualizowane!",
+				meet: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować spotkania`,
+				});
+			}
+		});
 };
 
 exports.deleteMeeting = (req, res, next) => {
-  const meetId = req.params.meetId;
-  MeetingRepository.deleteMeeting(meetId)
-    .then((result) => {
-      res.status(200).json({
-        message: 'Removed Meeting',
-        meet: result,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	const meetId = req.params.meetId;
+	MeetingRepository.deleteMeeting(meetId)
+		.then((result) => {
+			res.status(200).json({
+				message: "Spotkanie usunięto",
+				meet: result,
+			});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
--- model/sequelize/Meeting.js
+++ model/sequelize/Meeting.js
@@ -1,121 +1,163 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 const Op = Sequelize.Op;
 
-const Meeting = sequelize.define('Meeting', {
-	IdMeeting: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-	From: {
-		type: Sequelize.DATE, allowNull: true,
-		validate: {
-			isDate: {
-				msg: 'Pole powinno być prawidłową datą'
-			},
-			isAvailable(value, next) {
-				const roomId = this.IdRoom;
-				const toDate = this.To;
-				const meetingId = this.IdMeeting;
-				Meeting.findAll({
-					where: {
-						IdMeeting: {
-							[Op.ne]: meetingId
-						},  
-						[Op.or]: [
-							{
-								From: { [Op.lte]: value },
-								To: { [Op.gte]: value }
-							},
-							{
-								From: { [Op.gte]: value },
-								To: { [Op.lte]: toDate }
-							}
-						],
-						IdRoom: roomId
-
-					}
-				})
-					.then((meeting) => {
-						if (meeting.length != 0)
-							next(new Error(`Sala ${roomId} o godz. ${value} zajęta!`));
-						next();
-					})
-					.catch((onError) => console.log(onError));
-			}
-		}
-	},
-	To: {
-		type: Sequelize.DATE, allowNull: true,
-		validate: {
-			isDate: {
-				msg: 'Pole powinno być prawidłową datą'
-			},
-			isLessThanFrom(value) {
-				if (value <= this.From) {
-					throw new Error(`Data końcowa musi być późniejsza niż początkowa`);
-				}
-			},
-			isAvailable(value, next) {
-				const roomId = this.IdRoom;
-				const meetingId = this.IdMeeting;
-
-				Meeting.findAll({
-					where: {
-						IdMeeting: {
-							[Op.ne]: meetingId
-						},   
-						From: {
-							[Op.lte]: value
-						},
-						To: {
-							[Op.gte]: value
-						},
-						IdRoom: roomId
-					}
-				})
-					.then((meeting) => {
-						if (meeting.length != 0)
-							next(new Error(`Sala ${roomId} o godz. ${value} zajęta!`));
-						next();
-					})
-					.catch((onError) => console.log(onError));
-			}
-		}
-	},
-	IdGroup: { type: Sequelize.INTEGER, allowNull: true },
-	IdRoom: {
-		type: Sequelize.INTEGER, allowNull: true,
-		validate: {
-			isCapacity(value, next) {
-				const idGroup = this.IdGroup;
-				sequelize.models.EmployeeGroup.findAndCountAll({
-					where: {
-						IdGroup: idGroup
-					}
-				})
-					.then(group => {
-						sequelize.models.Room.findByPk(value)
-							.then(room => {
-								if (room.CapacitySet1 < group.count)
-									next(new Error(`Liczba osób w grupie zbyt duża dla wybranej sali!`));
-								next();
-							})
-							.catch(next);
-					})
-					.catch((onError) => console.log(onError));
-			}
-		}
-	}
-}, {
-	timestamps: false,
-	tableName: 'Meeting',
-	indexes: [
-		{
-			name: 'idx_meeting_idGroup',
-			fields: ['IdGroup']
+const Meeting = sequelize.define(
+	"Meeting",
+	{
+		IdMeeting: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
 		},
-		{
-			name: 'idx_meeting_idRoom',
-			fields: ['IdRoom']
-		}
-	]
-});
+		From: {
+			type: Sequelize.DATE,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy podać czas rozpoczęcia",
+				},
+				isDate: {
+					msg: "Pole powinno być prawidłową datą",
+				},
+				isAvailable(value, next) {
+					const roomId = this.IdRoom;
+					const toDate = this.To;
+					const meetingId = this.IdMeeting;
+					Meeting.findAll({
+						where: {
+							IdMeeting: {
+								[Op.ne]: meetingId,
+							},
+							[Op.or]: [
+								{
+									From: { [Op.lte]: value },
+									To: { [Op.gte]: value },
+								},
+								{
+									From: { [Op.gte]: value },
+									To: { [Op.lte]: toDate },
+								},
+							],
+							IdRoom: roomId,
+						},
+					})
+						.then((meeting) => {
+							if (meeting.length != 0)
+								next(
+									new Error(
+										`Sala ${roomId} o godz. ${value} zajęta!`
+									)
+								);
+							next();
+						})
+						.catch((onError) => console.log(onError));
+				},
+			},
+		},
+		To: {
+			type: Sequelize.DATE,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy podać czas zakończenia",
+				},
+				isDate: {
+					msg: "Pole powinno być prawidłową datą",
+				},
+				isLessThanFrom(value) {
+					if (value <= this.From) {
+						throw new Error(
+							`Data końcowa musi być późniejsza niż początkowa`
+						);
+					}
+				},
+				isAvailable(value, next) {
+					const roomId = this.IdRoom;
+					const meetingId = this.IdMeeting;
+
+					Meeting.findAll({
+						where: {
+							IdMeeting: {
+								[Op.ne]: meetingId,
+							},
+							From: {
+								[Op.lte]: value,
+							},
+							To: {
+								[Op.gte]: value,
+							},
+							IdRoom: roomId,
+						},
+					})
+						.then((meeting) => {
+							if (meeting.length != 0)
+								next(
+									new Error(
+										`Sala ${roomId} o godz. ${value} zajęta!`
+									)
+								);
+							next();
+						})
+						.catch((onError) => console.log(onError));
+				},
+			},
+		},
+		IdGroup: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy podać grupę",
+				},
+				isInt: {
+					msg: "Należy podać grupę",
+				},
+			},
+		},
+		IdRoom: {
+			type: Sequelize.INTEGER,
+			allowNull: true,
+			validate: {
+				isCapacity(value, next) {
+					const idGroup = this.IdGroup;
+					sequelize.models.EmployeeGroup.findAndCountAll({
+						where: {
+							IdGroup: idGroup,
+						},
+					})
+						.then((group) => {
+							sequelize.models.Room.findByPk(value)
+								.then((room) => {
+									if (room.CapacitySet1 < group.count)
+										next(
+											new Error(
+												`Liczba osób w grupie zbyt duża dla wybranej sali!`
+											)
+										);
+									next();
+								})
+								.catch(next);
+						})
+						.catch((onError) => console.log(onError));
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "Meeting",
+		indexes: [
+			{
+				name: "idx_meeting_idGroup",
+				fields: ["IdGroup"],
+			},
+			{
+				name: "idx_meeting_idRoom",
+				fields: ["IdRoom"],
+			},
+		],
+	}
+);
 module.exports = Meeting;
