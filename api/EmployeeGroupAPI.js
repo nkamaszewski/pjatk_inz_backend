89cb08bc69330306264b210eb8732a04c37df712
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-03 19:03:34 +0100

    poprawki walidacji i kom. błędów employeeGroup

--- api/EmployeeGroupAPI.js
+++ api/EmployeeGroupAPI.js
@@ -1,67 +1,107 @@
-const EmployeeRepository = require('../repository/sequelize/EmployeeRepository');
-const EmployeeGroupRepository = require('../repository/sequelize/EmployeeGroupRepository');
-const GroupRepository = require('../repository/sequelize/GroupRepository');
+const EmployeeRepository = require("../repository/sequelize/EmployeeRepository");
+const EmployeeGroupRepository = require("../repository/sequelize/EmployeeGroupRepository");
+const GroupRepository = require("../repository/sequelize/GroupRepository");
 
 exports.getEmployeeGroups = (req, res, next) => {
-  EmployeeGroupRepository.getEmployeeGroups()
-    .then((emps) => {
-      res.status(200).json(emps);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	EmployeeGroupRepository.getEmployeeGroups()
+		.then((emps) => {
+			res.status(200).json(emps);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getEmployeeGroupById = (req, res, next) => {
-  const empGroupId = req.params.empGroupId;
-  EmployeeGroupRepository.getEmployeeGroupById(empGroupId).then((emp) => {
-    if (!emp) {
-      res.status(404).json({
-        message: 'EmployeeGroup with id: ' + empGroupId + ' not found',
-      });
-    } else {
-      res.status(200).json(emp);
-    }
-  });
+	const empGroupId = req.params.empGroupId;
+	EmployeeGroupRepository.getEmployeeGroupById(empGroupId).then((emp) => {
+		if (!emp) {
+			res.status(404).json({
+				message: "EmployeeGroup with id: " + empGroupId + " not found",
+			});
+		} else {
+			res.status(200).json(emp);
+		}
+	});
 };
 
 exports.createEmployeeGroup = (req, res, next) => {
-  EmployeeGroupRepository.createEmployeeGroup(req.body)
-    .then((newObj) => {
-      res.status(201).json(newObj);
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	EmployeeGroupRepository.createEmployeeGroup(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Pracownik jest już przypisany do tej grupy`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować przypisania`,
+				});
+			}
+		});
 };
 
 exports.updateEmployeeGroup = (req, res, next) => {
-  const empGroupId = req.params.empGroupId;
-  EmployeeGroupRepository.updateEmployeeGroup(empGroupId, req.body)
-    .then((result) => {
-      res.status(200).json({ message: 'EmployeeGroup updated!', emp: result });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	const empGroupId = req.params.empGroupId;
+	EmployeeGroupRepository.updateEmployeeGroup(empGroupId, req.body)
+		.then((result) => {
+			res.status(200).json({
+				message: "EmployeeGroup updated!",
+				emp: result,
+			});
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Pracownik jest już przypisany do tej grupy`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować przypisania`,
+				});
+			}
+		});
 };
 
 exports.deleteEmployeeGroup = (req, res, next) => {
-  const empGrpId = req.params.empGrpId;
-  EmployeeGroupRepository.deleteEmployeeGroup(empGrpId)
-    .then((result) => {
-      res.status(200).json({ message: 'Removed EmployeeGroup', emp: result });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	const empGrpId = req.params.empGrpId;
+	EmployeeGroupRepository.deleteEmployeeGroup(empGrpId)
+		.then((result) => {
+			res.status(200).json({
+				message: "Usunięto przypisanie do grupy",
+				emp: result,
+			});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			res.status(403).json({
+				message: "Nie udało się usunąć przypisania!",
+			});
+		});
 };
--- model/sequelize/EmployeeGroup.js
+++ model/sequelize/EmployeeGroup.js
@@ -1,40 +1,71 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const EmployeeGroup = sequelize.define('EmployeeGroup', {
-    IdEmployeeGroup: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-    IdGroup: {
-        type: Sequelize.INTEGER, allowNull: false,
-        validate: {
-            isMaxOfPerson(value, next) {
-                EmployeeGroup.findAndCountAll({
-                    where: {
-                        IdGroup: value
-                    }
-                })
-                    .then((emp) => {
-                        sequelize.models.Group.findByPk(value)
-                            .then(group => {
-                                if (group.NumberOfPerson < emp.count)
-                                    next(new Error(`Przekroczono zadeklarowaną liczbę osób w grupie!`));
-                                next();
-                            })
-                            .catch(next);
-                    })
-                    .catch((onError) => console.log(onError));
-            }
-        }
-    },
-    IdPerson: { type: Sequelize.INTEGER, allowNull: false }
-}, {
-    timestamps: false,
-    tableName: 'EmployeeGroup',
-    indexes: [
-        {
-            name: 'idx_employeeGroup',
-            unique: true,
-            fields: ['IdGroup', 'IdPerson']
-        }
-    ]
-});
+const EmployeeGroup = sequelize.define(
+	"EmployeeGroup",
+	{
+		IdEmployeeGroup: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		IdGroup: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				isMaxOfPerson(value, next) {
+					EmployeeGroup.findAndCountAll({
+						where: {
+							IdGroup: value,
+						},
+					})
+						.then((emp) => {
+							sequelize.models.Group.findByPk(value)
+								.then((group) => {
+									if (group.NumberOfPerson < emp.count)
+										next(
+											new Error(
+												`Przekroczono zadeklarowaną liczbę osób w grupie!`
+											)
+										);
+									next();
+								})
+								.catch(next);
+						})
+						.catch((onError) => console.log(onError));
+				},
+				notNull: {
+					msg: "Należy podać grupę",
+				},
+				isInt: {
+					msg: "Należy podać grupę",
+				},
+			},
+		},
+		IdPerson: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy podać pracownika",
+				},
+				isInt: {
+					msg: "Należy podać pracownika",
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "EmployeeGroup",
+		indexes: [
+			{
+				name: "idx_employeeGroup",
+				unique: true,
+				fields: ["IdGroup", "IdPerson"],
+			},
+		],
+	}
+);
 module.exports = EmployeeGroup;
