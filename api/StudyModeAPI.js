164f2e371ce86950371771927ab18616f5fb81f7
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-04 20:55:26 +0100

    poprawki walidacji i obsł. błędów StudyMode

--- api/StudyModeAPI.js
+++ api/StudyModeAPI.js
@@ -1,66 +1,103 @@
-const StudyModeRepository = require('../repository/sequelize/StudyModeRepository');
+const StudyModeRepository = require("../repository/sequelize/StudyModeRepository");
 
 exports.getStudyModes = (req, res, next) => {
-    StudyModeRepository.getStudyModes()
-        .then(studMods => {
-            res.status(200).json(studMods);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	StudyModeRepository.getStudyModes()
+		.then((studMods) => {
+			res.status(200).json(studMods);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getStudyModeById = (req, res, next) => {
-    const studModId = req.params.studModId;
-    StudyModeRepository.getStudyModeById(studModId)
-        .then(studMod => {
-            if (!studMod) {
-                res.status(404).json({
-                    message: 'Study mode with id: ' + studModId + ' not found'
-                })
-            } else {
-                res.status(200).json(studMod);
-            }
-        });
+	const studModId = req.params.studModId;
+	StudyModeRepository.getStudyModeById(studModId).then((studMod) => {
+		if (!studMod) {
+			res.status(404).json({
+				message: "Study mode with id: " + studModId + " not found",
+			});
+		} else {
+			res.status(200).json(studMod);
+		}
+	});
 };
 
 exports.createStudyMode = (req, res, next) => {
-    StudyModeRepository.createStudyMode(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	StudyModeRepository.createStudyMode(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje tryb studiów o takiej nazwie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się utworzyć trybu studiów`,
+				});
+			}
+		});
 };
 
 exports.updateStudyMode = (req, res, next) => {
-    const studModId = req.params.studModId;
-    StudyModeRepository.updateStudyMode(studModId, req.body)
-        .then(result => {
-            res.status(200).json({ message: 'Study mode updated!', studMod: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const studModId = req.params.studModId;
+	StudyModeRepository.updateStudyMode(studModId, req.body)
+		.then((result) => {
+			res.status(200).json({ message: "Study mode updated!", studMod: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje tryb studiów o takiej nazwie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować trybu studiów`,
+				});
+			}
+		});
 };
 
 exports.deleteStudyMode = (req, res, next) => {
-    const studModId = req.params.studModId;
-    StudyModeRepository.deleteStudyMode(studModId)
-        .then(result => {
-            res.status(200).json({ message: 'Removed Study mode', studMod: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const studModId = req.params.studModId;
+	StudyModeRepository.deleteStudyMode(studModId)
+		.then((result) => {
+			res.status(200).json({ message: "Removed Study mode", studMod: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message: "Nie można usunąć trybu ze względu na przypisane studia",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć trybu studiów!",
+				});
+			}
+		});
 };
--- model/sequelize/StudyMode.js
+++ model/sequelize/StudyMode.js
@@ -1,11 +1,32 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const StudyMode = sequelize.define('StudyMode', {
-    IdStudyMode: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-    Name: { type: Sequelize.STRING, allowNull: false }
-}, {
-    timestamps: false,
-    tableName: 'StudyMode'
-});
+const StudyMode = sequelize.define(
+	"StudyMode",
+	{
+		IdStudyMode: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		Name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notNull: {
+					msg: "Należy podać nazwę trybu studiów",
+				},
+				notEmpty: {
+					msg: "Należy podać nazwę trybu studiów",
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "StudyMode",
+	}
+);
 module.exports = StudyMode;
