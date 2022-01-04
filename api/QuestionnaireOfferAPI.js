779577bca36ac88d03a34f8826844f5d9affd894
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-04 23:21:55 +0100

    poprawki walidacji i obsł. bł. questionnaireOffer

--- api/QuestionnaireOfferAPI.js
+++ api/QuestionnaireOfferAPI.js
@@ -1,124 +1,168 @@
-const QuestionnaireOfferRepository = require('../repository/sequelize/QuestionnaireOfferRepository');
+const QuestionnaireOfferRepository = require("../repository/sequelize/QuestionnaireOfferRepository");
 
 exports.getQuestionnaireOffers = (req, res, next) => {
-    const uId = req.userId;
-    const uIdDepartment = req.userIdDepartment;
-    const uIdDivision = req.userIdDivision;
-    const uIdRole = req.userIdRole;
+	const uId = req.userId;
+	const uIdDepartment = req.userIdDepartment;
+	const uIdDivision = req.userIdDivision;
+	const uIdRole = req.userIdRole;
 
-  QuestionnaireOfferRepository.getQuestionnaireOffers(uId,uIdDepartment,uIdDivision,uIdRole)
-    .then((questsoff) => {
-      res.status(200).json(questsoff);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	QuestionnaireOfferRepository.getQuestionnaireOffers(
+		uId,
+		uIdDepartment,
+		uIdDivision,
+		uIdRole
+	)
+		.then((questsoff) => {
+			res.status(200).json(questsoff);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getQuestionnaireOfferById = (req, res, next) => {
-  const questOffId = req.params.questOffId;
-  QuestionnaireOfferRepository.getQuestionnaireOfferById(questOffId).then(
-    (questoff) => {
-      if (!questoff) {
-        res.status(404).json({
-          message: 'Questionnaire Offer with id: ' + questOffId + ' not found',
-        });
-      } else {
-        res.status(200).json(questoff);
-      }
-    }
-  );
+	const questOffId = req.params.questOffId;
+	QuestionnaireOfferRepository.getQuestionnaireOfferById(questOffId).then(
+		(questoff) => {
+			if (!questoff) {
+				res.status(404).json({
+					message: "Questionnaire Offer with id: " + questOffId + " not found",
+				});
+			} else {
+				res.status(200).json(questoff);
+			}
+		}
+	);
 };
 
 exports.createQuestionnaireOffer = (req, res, next) => {
-  QuestionnaireOfferRepository.createQuestionnaireOffer(req.body)
-    .then((newObj) => {
-      res.status(201).json(newObj);
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	QuestionnaireOfferRepository.createQuestionnaireOffer(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje wniosek tego pracownika złożony w tym roku`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się utworzyć wniosku`,
+				});
+			}
+		});
 };
 
 exports.updateQuestionnaireOffer = (req, res, next) => {
-  const questoffId = req.params.questOffId;
-  QuestionnaireOfferRepository.updateQuestionnaireOffer(questoffId, req.body)
-    .then((result) => {
-      res
-        .status(200)
-        .json({ message: 'Questionnaire Offer updated!', questoff: result });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	const questoffId = req.params.questOffId;
+	QuestionnaireOfferRepository.updateQuestionnaireOffer(questoffId, req.body)
+		.then((result) => {
+			res
+				.status(200)
+				.json({ message: "Questionnaire Offer updated!", questoff: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje wniosek tego pracownika złożony w tym roku`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować wniosku`,
+				});
+			}
+		});
 };
 
 exports.deleteQuestionnaireOffer = (req, res, next) => {
-  const questoffId = req.params.questOffId;
-  const userId = req.userId;
+	const questoffId = req.params.questOffId;
+	const userId = req.userId;
 
-  QuestionnaireOfferRepository.deleteQuestionnaireOffer(questoffId, userId)
-    .then((result) => {
-      res
-        .status(200)
-        .json({ message: 'Removed Questionnaire Offer', questoff: result });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	QuestionnaireOfferRepository.deleteQuestionnaireOffer(questoffId, userId)
+		.then((result) => {
+			res
+				.status(200)
+				.json({ message: "Removed Questionnaire Offer", questoff: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message: "Nie można usunąć wniosku, który zawiera propozycje szkoleń",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć wniosku!",
+				});
+			}
+		});
 };
 
 exports.getQuestionnaireOfferByEmpId = (req, res, next) => {
-  const empId = req.params.empId;
-  QuestionnaireOfferRepository.getQuestionnaireOfferByEmpId(empId).then(
-    (questoff) => {
-      if (!questoff) {
-        res.status(404).json({
-          message: 'Questionnaire Offer with IdPerson: ' + empId + ' not found',
-        });
-      } else {
-        res.status(200).json(questoff);
-      }
-    }
-  );
+	const empId = req.params.empId;
+	QuestionnaireOfferRepository.getQuestionnaireOfferByEmpId(empId).then(
+		(questoff) => {
+			if (!questoff) {
+				res.status(404).json({
+					message: "Questionnaire Offer with IdPerson: " + empId + " not found",
+				});
+			} else {
+				res.status(200).json(questoff);
+			}
+		}
+	);
 };
 
 exports.getQuestionnaireOfferByDepId = (req, res, next) => {
-  const depId = req.params.depId;
-  QuestionnaireOfferRepository.getQuestionnaireOfferByDepId(depId).then(
-    (questoff) => {
-      if (!questoff) {
-        res.status(404).json({
-          message: 'Questionnaire Offer with IdDepartment: ' + depId + ' not found',
-        });
-      } else {
-        res.status(200).json(questoff);
-      }
-    }
-  );
+	const depId = req.params.depId;
+	QuestionnaireOfferRepository.getQuestionnaireOfferByDepId(depId).then(
+		(questoff) => {
+			if (!questoff) {
+				res.status(404).json({
+					message:
+						"Questionnaire Offer with IdDepartment: " + depId + " not found",
+				});
+			} else {
+				res.status(200).json(questoff);
+			}
+		}
+	);
 };
 
-
 exports.getQuestionnaireOfferByDivId = (req, res, next) => {
-  const divId = req.params.divId;
-  QuestionnaireOfferRepository.getQuestionnaireOfferByDepId(divId).then(
-    (questoff) => {
-      if (!questoff) {
-        res.status(404).json({
-          message: 'Questionnaire Offer with IdDivision: ' + divId + ' not found',
-        });
-      } else {
-        res.status(200).json(questoff);
-      }
-    }
-  );
+	const divId = req.params.divId;
+	QuestionnaireOfferRepository.getQuestionnaireOfferByDepId(divId).then(
+		(questoff) => {
+			if (!questoff) {
+				res.status(404).json({
+					message:
+						"Questionnaire Offer with IdDivision: " + divId + " not found",
+				});
+			} else {
+				res.status(200).json(questoff);
+			}
+		}
+	);
 };
--- model/sequelize/QuestionnaireOffer.js
+++ model/sequelize/QuestionnaireOffer.js
@@ -1,12 +1,56 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const QuestionnaireOffer = sequelize.define('QuestionnaireOffer', {
-    IdQuestionnaireOffer: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-    Year: { type: Sequelize.INTEGER, allowNull: false },
-    IdPerson: { type: Sequelize.INTEGER, allowNull: false }
-}, {
-    timestamps: false,
-    tableName: 'QuestionnaireOffer'
-});
+const QuestionnaireOffer = sequelize.define(
+	"QuestionnaireOffer",
+	{
+		IdQuestionnaireOffer: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		Year: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy podać rok",
+				},
+				notEmpty: {
+					msg: "Należy podać rok",
+				},
+				isNumeric: {
+					msg: "Rok powinien być liczbą",
+				},
+			},
+		},
+		IdPerson: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: "Należy wybrać pracownika",
+				},
+				notEmpty: {
+					msg: "Należy wybrać pracownika",
+				},
+				isNumeric: {
+					msg: "Należy wybrać pracownika",
+				},
+			},
+		},
+	},
+	{
+		timestamps: false,
+		tableName: "QuestionnaireOffer",
+		indexes: [
+			{
+				name: "idx_QuestionnaireOffer",
+				unique: true,
+				fields: ["IdPerson", "Year"],
+			},
+		],
+	}
+);
 module.exports = QuestionnaireOffer;
