14575d531a5f4787589589edf50be9644e4842f4
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-04 21:27:54 +0100

    poprawki walidacji i obsługi błędów university

--- api/UniversityAPI.js
+++ api/UniversityAPI.js
@@ -1,66 +1,104 @@
-const UniversityRepository = require('../repository/sequelize/UniversityRepository');
+const UniversityRepository = require("../repository/sequelize/UniversityRepository");
 
 exports.getUniversitys = (req, res, next) => {
-    UniversityRepository.getUniversitys()
-        .then(univs => {
-            res.status(200).json(univs);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	UniversityRepository.getUniversitys()
+		.then((univs) => {
+			res.status(200).json(univs);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getUniversityById = (req, res, next) => {
-    const univId = req.params.univId;
-    UniversityRepository.getDepartmentById(univId)
-        .then(univ => {
-            if (!univ) {
-                res.status(404).json({
-                    message: 'University with id: ' + univId + ' not found'
-                })
-            } else {
-                res.status(200).json(univ);
-            }
-        });
+	const univId = req.params.univId;
+	UniversityRepository.getDepartmentById(univId).then((univ) => {
+		if (!univ) {
+			res.status(404).json({
+				message: "University with id: " + univId + " not found",
+			});
+		} else {
+			res.status(200).json(univ);
+		}
+	});
 };
 
 exports.createUniversity = (req, res, next) => {
-    UniversityRepository.createUniversity(req.body)
-        .then(newObj => {
-            res.status(201).json(newObj);
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	UniversityRepository.createUniversity(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje uczelnia o takiej nazwie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się utworzyć uczelni`,
+				});
+			}
+		});
 };
 
 exports.updateUniversity = (req, res, next) => {
-    const univId = req.params.univId;
-    UniversityRepository.updateUniversity(univId, req.body)
-        .then(result => {
-            res.status(200).json({ message: 'University updated!', univ: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const univId = req.params.univId;
+	UniversityRepository.updateUniversity(univId, req.body)
+		.then((result) => {
+			res.status(200).json({ message: "University updated!", univ: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Istnieje uczelnia o takiej nazwie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować uczelni`,
+				});
+			}
+		});
 };
 
 exports.deleteUniversity = (req, res, next) => {
-    const univId = req.params.univId;
-    UniversityRepository.deleteUniversity(univId)
-        .then(result => {
-            res.status(200).json({ message: 'Removed University', univ: result });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+	const univId = req.params.univId;
+	UniversityRepository.deleteUniversity(univId)
+		.then((result) => {
+			res.status(200).json({ message: "Removed University", univ: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message:
+						"Nie można usunąć uczelni ze względu na przypisania do studiów",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć uczelni!",
+				});
+			}
+		});
 };
--- model/sequelize/University.js
+++ model/sequelize/University.js
@@ -1,38 +1,101 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
-const University = sequelize.define('University', {
-	IdUniversity: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
-	Name: {
-		type: Sequelize.STRING, allowNull: false, unique: true,
-		validate: {
-			notEmpty: {
-				msg: "Pole jest wymagane"
+const University = sequelize.define(
+	"University",
+	{
+		IdUniversity: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		Name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notEmpty: {
+					msg: "Należy podać nazwę uczelni",
+				},
+				notNull: {
+					msg: "Należy podać nazwę uczelni",
+				},
+				len: {
+					args: [5, 255],
+					msg: "Pole powinno zawierać od 5 do 255 znaków",
+				},
 			},
-			len: {
-				args: [5, 60],
-				msg: "Pole powinno zawierać od 5 do 255 znaków"
-			}
-		}
-	},
-	ShortName: {
-		type: Sequelize.STRING, allowNull: false, unique: true,
-		validate: {
-			notEmpty: {
-				msg: "Pole jest wymagane"
+		},
+		ShortName: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notEmpty: {
+					msg: "Należy podać nazwę uczelni",
+				},
+				notNull: {
+					msg: "Należy podać nazwę uczelni",
+				},
+				len: {
+					args: [2, 10],
+					msg: "Pole powinno zawierać od 2 do 10 znaków",
+				},
 			},
-			len: {
-				args: [2, 10],
-				msg: "Pole powinno zawierać od 2 do 10 znaków"
-			}
-		}
+		},
+		City: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy podać miasto",
+				},
+				notNull: {
+					msg: "Należy podać miasto",
+				},
+			},
+		},
+		PostalCode: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy podać kod pocztowy",
+				},
+				notNull: {
+					msg: "Należy podać kod pocztowy",
+				},
+			},
+		},
+		Street: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy podać ulicę",
+				},
+				notNull: {
+					msg: "Należy podać ulicę",
+				},
+			},
+		},
+		Number: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy podać numer",
+				},
+				notNull: {
+					msg: "Należy podać numer",
+				},
+			},
+		},
 	},
-	City: { type: Sequelize.STRING, allowNull: false },
-	PostalCode: { type: Sequelize.STRING, allowNull: false },
-	Street: { type: Sequelize.STRING, allowNull: false },
-	Number: { type: Sequelize.STRING, allowNull: false }
-}, {
-	timestamps: false,
-	tableName: 'University'
-});
+	{
+		timestamps: false,
+		tableName: "University",
+	}
+);
 module.exports = University;
