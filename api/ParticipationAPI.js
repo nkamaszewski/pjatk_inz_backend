65542e1797b862be49030fb11d70a26b5ee66788
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-05 00:38:37 +0100

    poprawki walidacji i obsł. błędów participation

--- api/ParticipationAPI.js
+++ api/ParticipationAPI.js
@@ -1,100 +1,130 @@
-const ParticipationRepository = require('../repository/sequelize/ParticipationRepository');
-const Role = require('../model/Role');
+const ParticipationRepository = require("../repository/sequelize/ParticipationRepository");
+const Role = require("../model/Role");
 
 exports.getParticipations = (req, res, next) => {
-  ParticipationRepository.getParticipations()
-    .then((particips) => {
-      res.status(200).json(particips);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	ParticipationRepository.getParticipations()
+		.then((particips) => {
+			res.status(200).json(particips);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getParticipationsByIdEducation = (req, res, next) => {
-  const { idEducation } = req.params;
-  ParticipationRepository.getParticipationsByIdEducation(idEducation)
-    .then((particips) => {
-      res.status(200).json(particips);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	const { idEducation } = req.params;
+	ParticipationRepository.getParticipationsByIdEducation(idEducation)
+		.then((particips) => {
+			res.status(200).json(particips);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getParticipationById = (req, res, next) => {
-  const participId = req.params.participId;
-  ParticipationRepository.getParticipationById(participId).then((particip) => {
-    if (!particip) {
-      res.status(404).json({
-        message: 'Participation with id: ' + participId + ' not found',
-      });
-    } else {
-      res.status(200).json(particip);
-    }
-  });
+	const participId = req.params.participId;
+	ParticipationRepository.getParticipationById(participId).then((particip) => {
+		if (!particip) {
+			res.status(404).json({
+				message: "Participation with id: " + participId + " not found",
+			});
+		} else {
+			res.status(200).json(particip);
+		}
+	});
 };
 
 exports.createParticipation = (req, res, next) => {
-
-  ParticipationRepository.createParticipation(req.body)
-    .then((newObj) => {
-      res.status(201).json(newObj);
-    })
-    .catch((err) => {
-      if (err.name === "SequelizeUniqueConstraintError") {
-          res.status(403).json({ message: "Użytkownik jest już zapisany na szkolenie"});
-      } else if(err.name === "SequelizeValidationError") {
-          res.status(403).json({ message: err.errors[0].message});
-      }
-      else if (!err.statusCode) {
-        err.statusCode = 500;
-        next(err);
-      }
-
-    });
+	ParticipationRepository.createParticipation(req.body)
+		.then((newObj) => {
+			res.status(201).json(newObj);
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Pracownik jest już zapisany na to szkolenie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się dodać zapisu na szkolenie`,
+				});
+			}
+		});
 };
 
 exports.updateParticipation = (req, res, next) => {
-  if (req.userIdRole != Role.ADMIN) {
-    res.status(403).json({
-        message: 'Brak uprawnień'
-    })
-}
-  const participId = req.params.participId;
-  ParticipationRepository.updateParticipation(participId, req.body)
-    .then((result) => {
-      res
-        .status(200)
-        .json({ message: 'Participation updated!', particip: result });
-    })
-    .catch((err) => {
-      if (err.name === "SequelizeUniqueConstraintError") {
-          res.status(403).json({ message: "Użytkownik jest już zapisany na szkolenie"});
-      } else if (!err.statusCode) {
-        err.statusCode = 500;
-        next(err);
-      }
-    });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	const participId = req.params.participId;
+	ParticipationRepository.updateParticipation(participId, req.body)
+		.then((result) => {
+			res
+				.status(200)
+				.json({ message: "Participation updated!", particip: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeUniqueConstraintError") {
+				res.status(403).json({
+					message: `Pracownik jest już zapisany na to szkolenie`,
+				});
+			} else if (err.name === "SequelizeValidationError") {
+				let message = "";
+				for (let m of err.errors) {
+					message += m.message + "\n";
+				}
+				res.status(403).json({
+					message,
+				});
+			} else {
+				if (!err.statusCode) {
+					err.statusCode = 500;
+				}
+				res.status(403).json({
+					message: `Nie udało się zaktualizować zapisu na szkolenie`,
+				});
+			}
+		});
 };
 
 exports.deleteParticipation = (req, res, next) => {
-  if (req.userIdRole != Role.ADMIN) {
-    res.status(403).json({
-        message: 'Brak uprawnień'
-    })
-}
-  const participId = req.params.participId;
-  ParticipationRepository.deleteParticipation(participId)
-    .then((result) => {
-      res
-        .status(200)
-        .json({ message: 'Removed Participation', particip: result });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+	if (req.userIdRole != Role.ADMIN) {
+		res.status(403).json({
+			message: "Brak uprawnień",
+		});
+	}
+	const participId = req.params.participId;
+	ParticipationRepository.deleteParticipation(participId)
+		.then((result) => {
+			res
+				.status(200)
+				.json({ message: "Removed Participation", particip: result });
+		})
+		.catch((err) => {
+			if (err.name === "SequelizeForeignKeyConstraintError") {
+				res.status(403).json({
+					message:
+						"Nie można usunąć zapisu na szkolenie ze względu na powiązania z innymi dokumentami",
+				});
+			} else {
+				err.statusCode = 500;
+				res.status(403).json({
+					message: "Nie udało się usunąć zapisu na szkolenie!",
+				});
+			}
+		});
 };
--- model/sequelize/Participation.js
+++ model/sequelize/Participation.js
@@ -1,53 +1,85 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../../config/sequelize/sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
 
 const Participation = sequelize.define(
-  'Participation',
-  {
-    IdParticipation: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true,
-    },
-    IdPerson: { type: Sequelize.INTEGER, allowNull: false },
-    IdEducation: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      validate: {
-      	isAccepted(value, next) {
-      		const personId = this.IdPerson;
-      		sequelize.models.ApplicationFor.findOne({
-      			where: {
-      				IdPerson: personId,
-      				IdEducation: value,
-      				IdStatus: 5
-      			}
-      		})
-      			.then(applications => {
-            if(!applications) {
-                next(new Error(`Wniosek na to szkolenie nie zaakceptowany!`));
-              }
-              next()
-            })
-      			.catch((onError) => console.log(onError));
-      	}
-      }
-    },
-    DateOfRegistration: { type: Sequelize.DATE, allowNull: false },
-    EndDate: { type: Sequelize.DATE, allowNull: true },
-    CertificateOfCompletion: { type: Sequelize.STRING, allowNull: true },
-  },
-  {
-    timestamps: false,
-    tableName: 'Participation',
-    indexes: [
-      {
-        name: 'idx_participation_idPerson_idEducation',
-        unique: true,
-        fields: ['IdPerson', 'IdEducation'],
-     },
-    ],
-  }
+	"Participation",
+	{
+		IdParticipation: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		IdPerson: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy wybrać pracownika",
+				},
+				isNumeric: {
+					msg: "Należy wybrać pracownika",
+				},
+			},
+		},
+		IdEducation: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: "Należy wybrać szkolenie",
+				},
+				isNumeric: {
+					msg: "Należy wybrać szkolenie",
+				},
+				isAccepted(value, next) {
+					const personId = this.IdPerson;
+					sequelize.models.ApplicationFor.findOne({
+						where: {
+							IdPerson: personId,
+							IdEducation: value,
+							IdStatus: 5,
+						},
+					})
+						.then((applications) => {
+							if (!applications) {
+								next(new Error(`Wniosek na to szkolenie nie zaakceptowany!`));
+							}
+							next();
+						})
+						.catch((onError) => console.log(onError));
+				},
+			},
+		},
+		DateOfRegistration: {
+			type: Sequelize.DATE,
+			allowNull: false,
+			defaultValue: Sequelize.NOW,
+			validate: {
+				notNull: {
+					msg: "Należy wstawić datę początkową zatrudnienia",
+				},
+				notEmpty: {
+					msg: "Należy wstawić datę początkową zatrudnienia",
+				},
+				isDate: {
+					msg: "Pole powinno być prawidłową datą",
+				},
+			},
+		},
+		EndDate: { type: Sequelize.DATE, allowNull: true },
+		CertificateOfCompletion: { type: Sequelize.STRING, allowNull: true },
+	},
+	{
+		timestamps: false,
+		tableName: "Participation",
+		indexes: [
+			{
+				name: "idx_participation_idPerson_idEducation",
+				unique: true,
+				fields: ["IdPerson", "IdEducation"],
+			},
+		],
+	}
 );
 module.exports = Participation;
