d4118e2f7abb752994de4d0ce754d1d9b29021d4
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-11 00:30:23 +0100

    dodane allowNull employment

--- repository/sequelize/EmployeeGroupRepository.js
+++ repository/sequelize/EmployeeGroupRepository.js
@@ -3,45 +3,43 @@ const Employee = require('../../model/sequelize/Employee');
 const EmployeeGroup = require('../../model/sequelize/EmployeeGroup');
 
 exports.getEmployeeGroups = () => {
-    return EmployeeGroup.findAll({
-        attributes: ['IdEmployeeGroup'],
-        include: [{
-            model: Employee,
-            as: 'employeeGroupEmployee'
-        },
-        {
-            model: Group,
-            as: 'employeeGroupGroup'
-        }]
-    });
-    // return EmployeeGroup.findAll();
-
+	return EmployeeGroup.findAll({
+		attributes: ['IdEmployeeGroup'],
+		include: [
+			{
+				model: Employee,
+				as: 'employeeGroupEmployee',
+			},
+			{
+				model: Group,
+				as: 'employeeGroupGroup',
+			},
+		],
+	});
 };
 
 exports.createEmployeeGroup = (newEmployeeGroupData) => {
-    return EmployeeGroup.create({
-        IdEmployeeGroup: newEmployeeGroupData.IdEmployeeGroup,
-        IdPerson: newEmployeeGroupData.IdPerson,
-        IdGroup: newEmployeeGroupData.IdGroup
-    });
+	return EmployeeGroup.create({
+		IdEmployeeGroup: newEmployeeGroupData.IdEmployeeGroup,
+		IdPerson: newEmployeeGroupData.IdPerson,
+		IdGroup: newEmployeeGroupData.IdGroup,
+	});
 };
 
 exports.deleteEmployeeGroup = (empGroupId) => {
-    return EmployeeGroup.destroy({
-        where: { IdEmployeeGroup: empGroupId }
-    });
+	return EmployeeGroup.destroy({
+		where: { IdEmployeeGroup: empGroupId },
+	});
 };
 
 exports.updateEmployeeGroup = (empGroupId, data) => {
+	const IdEmployeeGroup = data.IdEmployeeGroup;
+	const IdPerson = data.IdPerson;
+	const IdGroup = data.IdGroup;
 
-    const IdEmployeeGroup = data.IdEmployeeGroup;
-    const IdPerson = data.IdPerson;
-    const IdGroup = data.IdGroup;
-
-    return EmployeeGroup.update(data, { where: { IdEmployeeGroup: empGroupId } });
-}
-
-exports.getEmployeeGroupById = (empGroupId) => {
-    return EmployeeGroup.findByPk(empGroupId);
+	return EmployeeGroup.update(data, { where: { IdEmployeeGroup: empGroupId } });
 };
 
+exports.getEmployeeGroupById = (empGroupId) => {
+	return EmployeeGroup.findByPk(empGroupId);
+};
