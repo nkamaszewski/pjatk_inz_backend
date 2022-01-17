fa7a14b7c8ada00008816e421df908ad2349c3e0
Norbert Kamaszewski <norbert.kamaszewski@gmail.com>
2022-01-17 11:30:00 +0100

    update division get

--- api/DivisionAPI.js
+++ api/DivisionAPI.js
@@ -2,121 +2,121 @@ const DivisionRepository = require('../repository/sequelize/DivisionRepository')
 const Role = require('../model/Role');
 
 exports.getDivisions = (req, res, next) => {
-	DivisionRepository.getDivisions()
-		.then((divs) => {
-			res.status(200).json(divs);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  DivisionRepository.getDivisions()
+    .then((divs) => {
+      res.status(200).json(divs);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getDivisionById = (req, res, next) => {
-	const divId = req.params.divId;
-	DivisionRepository.getDivisionById(divId).then((div) => {
-		if (!div) {
-			res.status(404).json({
-				message: 'Division with id: ' + divId + ' not found',
-			});
-		} else {
-			res.status(200).json(div);
-		}
-	});
+  const divId = req.params.divId;
+  DivisionRepository.getDivisionById(divId).then((div) => {
+    if (!div) {
+      res.status(404).json({
+        message: 'Division with id: ' + divId + ' not found',
+      });
+    } else {
+      res.status(200).json(div);
+    }
+  });
 };
 
 exports.createDivision = (req, res, next) => {
-	if (req.userIdRole != Role.ADMIN) {
-		res.status(403).json({
-			message: 'Brak uprawnień',
-		});
-	} else {
-		DivisionRepository.createDivision(req.body)
-			.then((newObj) => {
-				res.status(201).json(newObj);
-			})
-			.catch((err) => {
-				if (err.name === 'SequelizeUniqueConstraintError') {
-					res.status(403).json({
-						message: `Istnieje pion o takiej nazwie`,
-					});
-				} else if (err.name === 'SequelizeValidationError') {
-					let message = '';
-					for (let m of err.errors) {
-						message += m.message + '\n';
-					}
-					res.status(403).json({
-						message,
-					});
-				} else {
-					if (!err.statusCode) {
-						err.statusCode = 500;
-					}
-					res.status(403).json({
-						message: `Nie udało się utworzyć pionu`,
-					});
-				}
-			});
-	}
+  if (req.userIdRole != Role.ADMIN) {
+    res.status(403).json({
+      message: 'Brak uprawnień',
+    });
+  } else {
+    DivisionRepository.createDivision(req.body)
+      .then((newObj) => {
+        res.status(201).json(newObj);
+      })
+      .catch((err) => {
+        if (err.name === 'SequelizeUniqueConstraintError') {
+          res.status(403).json({
+            message: `Istnieje pion o takiej nazwie`,
+          });
+        } else if (err.name === 'SequelizeValidationError') {
+          let message = '';
+          for (let m of err.errors) {
+            message += m.message + '\n';
+          }
+          res.status(403).json({
+            message,
+          });
+        } else {
+          if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          res.status(403).json({
+            message: `Nie udało się utworzyć pionu`,
+          });
+        }
+      });
+  }
 };
 
 exports.updateDivision = (req, res, next) => {
-	if (req.userIdRole != Role.ADMIN) {
-		res.status(403).json({
-			message: 'Brak uprawnień',
-		});
-	} else {
-		const divId = req.params.divId;
-		DivisionRepository.updateDivision(divId, req.body)
-			.then((result) => {
-				res.status(200).json({ message: 'Division updated!', div: result });
-			})
-			.catch((err) => {
-				if (err.name === 'SequelizeUniqueConstraintError') {
-					res.status(403).json({
-						message: `Istnieje pion o takiej nazwie`,
-					});
-				} else if (err.name === 'SequelizeValidationError') {
-					let message = '';
-					for (let m of err.errors) {
-						message += m.message + '\n';
-					}
-					res.status(403).json({
-						message,
-					});
-				} else {
-					if (!err.statusCode) {
-						err.statusCode = 500;
-					}
-					res.status(403).json({
-						message: `Nie udało się utworzyć pionu`,
-					});
-				}
-			});
-	}
+  if (req.userIdRole != Role.ADMIN) {
+    res.status(403).json({
+      message: 'Brak uprawnień',
+    });
+  } else {
+    const divId = req.params.divId;
+    DivisionRepository.updateDivision(divId, req.body)
+      .then((result) => {
+        res.status(200).json({ message: 'Division updated!', div: result });
+      })
+      .catch((err) => {
+        if (err.name === 'SequelizeUniqueConstraintError') {
+          res.status(403).json({
+            message: `Istnieje pion o takiej nazwie`,
+          });
+        } else if (err.name === 'SequelizeValidationError') {
+          let message = '';
+          for (let m of err.errors) {
+            message += m.message + '\n';
+          }
+          res.status(403).json({
+            message,
+          });
+        } else {
+          if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          res.status(403).json({
+            message: `Nie udało się utworzyć pionu`,
+          });
+        }
+      });
+  }
 };
 
 exports.deleteDivision = (req, res, next) => {
-	if (req.userIdRole != Role.ADMIN) {
-		res.status(403).json({
-			message: 'Brak uprawnień',
-		});
-	} else {
-		const divId = req.params.divId;
-		DivisionRepository.deleteDivision(divId)
-			.then((result) => {
-				res.status(200).json({ message: 'Pion usunięto', div: result });
-			})
-			.catch((err) => {
-				if (err.name === 'SequelizeForeignKeyConstraintError') {
-					res.status(403).json({
-						message: 'Nie można usunąć pionu ze względu na przypisane wydziały',
-					});
-				} else {
-					err.statusCode = 500;
-					res.status(403).json({
-						message: 'Nie udało się usunąć pionu!',
-					});
-				}
-			});
-	}
+  if (req.userIdRole != Role.ADMIN) {
+    res.status(403).json({
+      message: 'Brak uprawnień',
+    });
+  } else {
+    const divId = req.params.divId;
+    DivisionRepository.deleteDivision(divId)
+      .then((result) => {
+        res.status(200).json({ message: 'Pion usunięto', div: result });
+      })
+      .catch((err) => {
+        if (err.name === 'SequelizeForeignKeyConstraintError') {
+          res.status(403).json({
+            message: 'Nie można usunąć pionu ze względu na przypisane wydziały',
+          });
+        } else {
+          err.statusCode = 500;
+          res.status(403).json({
+            message: 'Nie udało się usunąć pionu!',
+          });
+        }
+      });
+  }
 };
--- repository/sequelize/DivisionRepository.js
+++ repository/sequelize/DivisionRepository.js
@@ -2,33 +2,38 @@ const Division = require('../../model/sequelize/Division');
 const Department = require('../../model/sequelize/Department');
 
 exports.getDivisions = () => {
-    return Division.findAll();
+  return Division.findAll({
+    include: {
+      model: Department,
+      as: 'divisionDepartments',
+    },
+  });
 };
 
 exports.createDivision = (newDivisionData) => {
-    return Division.create({
-        Name: newDivisionData.Name
-    });
+  return Division.create({
+    Name: newDivisionData.Name,
+  });
 };
 
 exports.deleteDivision = (divisionId) => {
-    return Division.destroy({
-        where: { IdDivision: divisionId }
-    });
+  return Division.destroy({
+    where: { IdDivision: divisionId },
+  });
 };
 
 exports.updateDivision = (divisionId, data) => {
-    const Name = data.Name;
-    return Division.update(data, { where: { IdDivision: divisionId } });
-}
-
-exports.getDivisionById = (divId) => {
-    return Division.findByPk(divId,
-        {
-            include: [{
-                model: Department,
-                as: 'divisionDepartments'
-            }]
-        });
+  const Name = data.Name;
+  return Division.update(data, { where: { IdDivision: divisionId } });
 };
 
+exports.getDivisionById = (divId) => {
+  return Division.findByPk(divId, {
+    include: [
+      {
+        model: Department,
+        as: 'divisionDepartments',
+      },
+    ],
+  });
+};
