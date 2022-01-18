df9020fae3e9bfa53034638bb33b2f6c837f5c86
Norbert Kamaszewski <norbert.kamaszewski@gmail.com>
2022-01-19 00:46:39 +0100

    edycja uczestnictwa

--- api/ParticipationAPI.js
+++ api/ParticipationAPI.js
@@ -3,185 +3,190 @@ const ParticipationRepositoryMySql2 = require('../repository/mysql2/Participatio
 const Role = require('../model/Role');
 
 exports.getParticipations = (req, res, next) => {
-	ParticipationRepository.getParticipations()
-		.then((particips) => {
-			res.status(200).json(particips);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  ParticipationRepository.getParticipations()
+    .then((particips) => {
+      res.status(200).json(particips);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getParticipationsByIdEducation = (req, res, next) => {
-	const { idEducation } = req.params;
-	ParticipationRepository.getParticipationsByIdEducation(idEducation)
-		.then((particips) => {
-			res.status(200).json(particips);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  const { idEducation } = req.params;
+  ParticipationRepository.getParticipationsByIdEducation(idEducation)
+    .then((particips) => {
+      res.status(200).json(particips);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getParticipationById = (req, res, next) => {
-	const participId = req.params.participId;
-	ParticipationRepository.getParticipationById(participId).then((particip) => {
-		if (!particip) {
-			res.status(404).json({
-				message: 'Participation with id: ' + participId + ' not found',
-			});
-		} else {
-			res.status(200).json(particip);
-		}
-	});
+  const participId = req.params.participId;
+  ParticipationRepository.getParticipationById(participId).then((particip) => {
+    if (!particip) {
+      res.status(404).json({
+        message: 'Participation with id: ' + participId + ' not found',
+      });
+    } else {
+      res.status(200).json(particip);
+    }
+  });
 };
 
 exports.createParticipation = (req, res, next) => {
-	if (req.userIdRole != Role.ADMIN) {
-		res.status(403).json({
-			message: 'Brak uprawnień',
-		});
-	} else {
-		if (req.body.EndDate == '') req.body.EndDate = null;
-		let buffer = null;
-		if (req.file) {
-			buffer = req.file.buffer;
-		}
-		const data = {
-			...req.body,
-			CertificateOfCompletion: buffer,
-		};
-		ParticipationRepository.createParticipation(data)
-			.then((newObj) => {
-				res.status(201).json(newObj);
-			})
-			.catch((err) => {
-				if (err.name === 'SequelizeUniqueConstraintError') {
-					res.status(403).json({
-						message: `Pracownik jest już zapisany na to szkolenie`,
-					});
-				} else if (err.name === 'SequelizeValidationError') {
-					let message = '';
-					for (let m of err.errors) {
-						message += m.message + '\n';
-					}
-					res.status(403).json({
-						message,
-					});
-				} else {
-					if (!err.statusCode) {
-						err.statusCode = 500;
-					}
-					res.status(403).json({
-						message: `Nie udało się dodać zapisu na szkolenie`,
-					});
-				}
-				// });
-			});
-	}
+  if (req.userIdRole != Role.ADMIN) {
+    res.status(403).json({
+      message: 'Brak uprawnień',
+    });
+  } else {
+    if (!req.body.EndDate) req.body.EndDate = null;
+    let buffer = null;
+    if (req.file) {
+      buffer = req.file.buffer;
+    }
+    const data = {
+      ...req.body,
+      CertificateOfCompletion: buffer,
+    };
+    ParticipationRepository.createParticipation(data)
+      .then((newObj) => {
+        res.status(201).json(newObj);
+      })
+      .catch((err) => {
+        if (err.name === 'SequelizeUniqueConstraintError') {
+          res.status(403).json({
+            message: `Pracownik jest już zapisany na to szkolenie`,
+          });
+        } else if (err.name === 'SequelizeValidationError') {
+          let message = '';
+          for (let m of err.errors) {
+            message += m.message + '\n';
+          }
+          res.status(403).json({
+            message,
+          });
+        } else {
+          if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          res.status(403).json({
+            message: `Nie udało się dodać zapisu na szkolenie`,
+          });
+        }
+        // });
+      });
+  }
 };
 
-exports.updateParticipation = (req, res, next) => {
-	if (req.userIdRole != Role.ADMIN) {
-		res.status(403).json({
-			message: 'Brak uprawnień',
-		});
-	} else {
-		const participId = req.params.participId;
-		if (req.body.EndDate == '') req.body.EndDate = null;
-		let buffer = null;
-		if (req.file) {
-			buffer = req.file.buffer;
-		}
-		const data = {
-			...req.body,
-			CertificateOfCompletion: buffer,
-		};
-		ParticipationRepository.updateParticipation(participId, data)
-			.then((result) => {
-				res
-					.status(200)
-					.json({ message: 'Participation updated!', particip: result });
-			})
-			.catch((err) => {
-				if (err.name === 'SequelizeUniqueConstraintError') {
-					res.status(403).json({
-						message: `Pracownik jest już zapisany na to szkolenie`,
-					});
-				} else if (err.name === 'SequelizeValidationError') {
-					let message = '';
-					for (let m of err.errors) {
-						message += m.message + '\n';
-					}
-					res.status(403).json({
-						message,
-					});
-				} else {
-					if (!err.statusCode) {
-						err.statusCode = 500;
-					}
-					res.status(403).json({
-						message: `Nie udało się zaktualizować zapisu na szkolenie`,
-					});
-				}
-			});
-	}
+exports.updateParticipation = async (req, res, next) => {
+  if (req.userIdRole != Role.ADMIN) {
+    res.status(403).json({
+      message: 'Brak uprawnień',
+    });
+  } else {
+    const participId = req.params.participId;
+    if (!req.body.EndDate) req.body.EndDate = null;
+    const data = {
+      ...req.body,
+    };
+    if (!req.file) {
+      const blob = await ParticipationRepository.getCertificateByPartId(
+        participId
+      );
+      const file = blob.dataValues.CertificateOfCompletion;
+      data.CertificateOfCompletion = file.buffer;
+    } else {
+      data.CertificateOfCompletion = req.file.buffer;
+    }
+
+    ParticipationRepository.updateParticipation(participId, data)
+      .then((result) => {
+        res
+          .status(200)
+          .json({ message: 'Participation updated!', particip: result });
+      })
+      .catch((err) => {
+        if (err.name === 'SequelizeUniqueConstraintError') {
+          res.status(403).json({
+            message: `Pracownik jest już zapisany na to szkolenie`,
+          });
+        } else if (err.name === 'SequelizeValidationError') {
+          let message = '';
+          for (let m of err.errors) {
+            message += m.message + '\n';
+          }
+          res.status(403).json({
+            message,
+          });
+        } else {
+          if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          res.status(403).json({
+            message: `Nie udało się zaktualizować zapisu na szkolenie`,
+          });
+        }
+      });
+  }
 };
 
 exports.deleteParticipation = (req, res, next) => {
-	if (req.userIdRole != Role.ADMIN) {
-		res.status(403).json({
-			message: 'Brak uprawnień',
-		});
-	} else {
-		const participId = req.params.participId;
-		ParticipationRepository.deleteParticipation(participId)
-			.then((result) => {
-				res
-					.status(200)
-					.json({ message: 'Removed Participation', particip: result });
-			})
-			.catch((err) => {
-				if (err.name === 'SequelizeForeignKeyConstraintError') {
-					res.status(403).json({
-						message:
-							'Nie można usunąć zapisu na szkolenie ze względu na powiązania z innymi dokumentami',
-					});
-				} else {
-					err.statusCode = 500;
-					res.status(403).json({
-						message: 'Nie udało się usunąć zapisu na szkolenie!',
-					});
-				}
-			});
-	}
+  if (req.userIdRole != Role.ADMIN) {
+    res.status(403).json({
+      message: 'Brak uprawnień',
+    });
+  } else {
+    const participId = req.params.participId;
+    ParticipationRepository.deleteParticipation(participId)
+      .then((result) => {
+        res
+          .status(200)
+          .json({ message: 'Removed Participation', particip: result });
+      })
+      .catch((err) => {
+        if (err.name === 'SequelizeForeignKeyConstraintError') {
+          res.status(403).json({
+            message:
+              'Nie można usunąć zapisu na szkolenie ze względu na powiązania z innymi dokumentami',
+          });
+        } else {
+          err.statusCode = 500;
+          res.status(403).json({
+            message: 'Nie udało się usunąć zapisu na szkolenie!',
+          });
+        }
+      });
+  }
 };
 
 exports.getCertificateByPartId = (req, res, next) => {
-	const participId = req.params.participId;
-	ParticipationRepository.getCertificateByPartId(participId).then(
-		(particip) => {
-			if (!particip) {
-				res.status(404).json({
-					message: 'Participation with id: ' + participId + ' not found',
-				});
-			} else {
-				const file = particip.dataValues.CertificateOfCompletion;
-				console.log(file);
-				res.status(200).end(Buffer.from(file, 'binary'));
-			}
-		}
-	);
+  const participId = req.params.participId;
+  ParticipationRepository.getCertificateByPartId(participId).then(
+    (particip) => {
+      if (!particip) {
+        res.status(404).json({
+          message: 'Participation with id: ' + participId + ' not found',
+        });
+      } else {
+        const file = particip.dataValues.CertificateOfCompletion;
+        console.log(file);
+        res.status(200).end(Buffer.from(file, 'binary'));
+      }
+    }
+  );
 };
 
 exports.getParticipationsWithoutQuest = (req, res, next) => {
-	const uId = req.userId;
+  const uId = req.userId;
 
-	ParticipationRepositoryMySql2.getParticipationsWithoutQuest(uId)
-		.then((particip) => {
-			res.status(200).json(particip);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  ParticipationRepositoryMySql2.getParticipationsWithoutQuest(uId)
+    .then((particip) => {
+      res.status(200).json(particip);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- routes/api/ParticipationAPIRoute.js
+++ routes/api/ParticipationAPIRoute.js
@@ -5,24 +5,28 @@ const multer = require('multer');
 const participApiController = require('../../api/ParticipationAPI');
 router.get('/', participApiController.getParticipations);
 router.get(
-	'/withoutquest/',
-	participApiController.getParticipationsWithoutQuest
+  '/withoutquest/',
+  participApiController.getParticipationsWithoutQuest
 );
 
 router.get('/:participId', participApiController.getParticipationById);
 router.get(
-	'/:idEducation/education',
-	participApiController.getParticipationsByIdEducation
+  '/:idEducation/education',
+  participApiController.getParticipationsByIdEducation
 );
 router.get(
-	'/:participId/certificate',
-	participApiController.getCertificateByPartId
+  '/:participId/certificate',
+  participApiController.getCertificateByPartId
 );
 router.post(
-	'/',
-	multer().single('CertificateOfCompletion'),
-	participApiController.createParticipation
+  '/',
+  multer().single('CertificateOfCompletion'),
+  participApiController.createParticipation
 );
-router.put('/:participId', participApiController.updateParticipation);
+router.put(
+  '/:participId',
+  multer().single('CertificateOfCompletion'),
+  participApiController.updateParticipation
+);
 router.delete('/:participId', participApiController.deleteParticipation);
 module.exports = router;
