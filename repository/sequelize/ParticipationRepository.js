6916ea8ffb761b2214616fd3aa9f8d271341f3d7
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-31 00:09:10 +0100

    dodane upraw. do wysłania certyfikatu dla pracow.

--- repository/sequelize/ParticipationRepository.js
+++ repository/sequelize/ParticipationRepository.js
@@ -3,84 +3,93 @@ const Employee = require('../../model/sequelize/Employee');
 const Person = require('../../model/sequelize/Person');
 
 exports.getParticipations = () => {
-  return Participation.findAll({
-    attributes: [
-      'IdParticipation',
-      'IdPerson',
-      'IdEducation',
-      'DateOfRegistration',
-      'EndDate',
-    ],
-    include: [
-      {
-        model: Employee,
-        as: 'participationEmployee',
-      },
-    ],
-  });
+	return Participation.findAll({
+		attributes: [
+			'IdParticipation',
+			'IdPerson',
+			'IdEducation',
+			'DateOfRegistration',
+			'EndDate',
+		],
+		include: [
+			{
+				model: Employee,
+				as: 'participationEmployee',
+			},
+		],
+	});
 };
 
 exports.getParticipationsByIdEducation = (IdEducation) => {
-  return Participation.findAll({
-    attributes: [
-      'IdParticipation',
-      'IdPerson',
-      'IdEducation',
-      'DateOfRegistration',
-      'EndDate',
-      'CertificateOfCompletion',
-    ],
-    where: { IdEducation },
-    include: [
-      {
-        model: Employee,
-        as: 'participationEmployee',
-        include: [
-          {
-            model: Person,
-            as: 'employeePerson',
-          },
-        ],
-      },
-    ],
-  });
+	return Participation.findAll({
+		attributes: [
+			'IdParticipation',
+			'IdPerson',
+			'IdEducation',
+			'DateOfRegistration',
+			'EndDate',
+			'CertificateOfCompletion',
+		],
+		where: { IdEducation },
+		include: [
+			{
+				model: Employee,
+				as: 'participationEmployee',
+				include: [
+					{
+						model: Person,
+						as: 'employeePerson',
+					},
+				],
+			},
+		],
+	});
 };
 
 exports.createParticipation = (newParticipationData) => {
-  return Participation.create({
-    DateOfRegistration: newParticipationData.DateOfRegistration,
-    EndDate: newParticipationData.EndDate,
-    CertificateOfCompletion: newParticipationData.CertificateOfCompletion,
-    IdPerson: newParticipationData.IdPerson,
-    IdEducation: newParticipationData.IdEducation,
-  });
+	return Participation.create({
+		DateOfRegistration: newParticipationData.DateOfRegistration,
+		EndDate: newParticipationData.EndDate,
+		CertificateOfCompletion: newParticipationData.CertificateOfCompletion,
+		IdPerson: newParticipationData.IdPerson,
+		IdEducation: newParticipationData.IdEducation,
+	});
 };
 
 exports.deleteParticipation = (participationId) => {
-  return Participation.destroy({
-    where: { IdParticipation: participationId },
-  });
+	return Participation.destroy({
+		where: { IdParticipation: participationId },
+	});
 };
 
 exports.updateParticipation = (participationId, data) => {
-  return Participation.update(data, {
-    where: { IdParticipation: participationId },
-  });
+	return Participation.update(data, {
+		where: { IdParticipation: participationId },
+	});
+};
+
+exports.updateParticipationUser = (participationId, data) => {
+	return Participation.update(
+		{ CertificateOfCompletion: data.CertificateOfCompletion },
+		{
+			where: { IdParticipation: participationId },
+		}
+	);
 };
 
 exports.getParticipationById = (participId) => {
-  return Participation.findByPk(participId);
+	return Participation.findByPk(participId);
 };
 
 exports.getParticipationByEduId = (eduId) => {
-  return Participation.findAndCountAll({
-    where: { IdEducation: eduId },
-  });
+	return Participation.findAndCountAll({
+		where: { IdEducation: eduId },
+	});
 };
 
 exports.getCertificateByPartId = (participId) => {
-  return Participation.findOne({
-    attributes: ['CertificateOfCompletion'],
-    where: { IdParticipation: participId },
-  });
+	return Participation.findOne({
+		attributes: ['CertificateOfCompletion'],
+		where: { IdParticipation: participId },
+	});
 };
